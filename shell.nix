daf893509d220d11d4752856bbcd96bf168d7776
Tony Olagbaiye <me@fron.io>
2020-03-27 08:30:51 +0000

    envrc: use nixos-rebuild in shell.nix

--- shell.nix
+++ shell.nix
@@ -5,35 +5,14 @@ let
   build = "config.system.build";
 
   rebuild = pkgs.writeShellScriptBin "rebuild" ''
-    if [[ -z $1 ]]; then
-      echo "Usage: $0 [--show-trace] [host] {switch|boot|test|iso}"
+    INITIAL_SYSTEM="$(readlink -f /run/current-system)"
+    sudo nixos-rebuild --flake $PWD $@
+    FINAL_SYSTEM="$(readlink -f /run/current-system)"
+
+    if [[ "$INITIAL_SYSTEM" != "$FINAL_SYSTEM" ]]; then
+      git tag $(basename $FINAL_SYSTEM)
     else
-      NIX="nix"
-      if [[ $1 == "--show-trace" ]]; then
-        NIX="nix -vv $1"
-        shift
-      fi
-
-      INITIAL_SYSTEM="$(readlink -f /run/current-system)"
-      if [[ $1 == "iso" ]]; then
-        $NIX build ${configs}.niximg.${build}.isoImage
-      elif [[ -z $2 ]]; then
-        sudo -E $NIX run ${configs}.${hostname}.${build}.toplevel -c switch-to-configuration $1
-        if [[ $INITIAL_SYSTEM != "$(readlink -f /run/current-system)" ]] \
-        && [[ $1 == "switch" ]]; then
-          sudo -E nix-env -p /nix/var/nix/profiles/system --set /run/current-system
-          sudo -E $NIX run ${configs}.${hostname}.${build}.toplevel -c switch-to-configuration boot
-        fi
-      else
-        sudo -E $NIX run -vv ${configs}.$1.${build}.toplevel -c switch-to-configuration $2
-      fi
-      FINAL_SYSTEM="$(readlink -f /run/current-system)"
-
-      if [[ "$INITIAL_SYSTEM" != "$FINAL_SYSTEM" ]]; then
-        git tag $(basename $FINAL_SYSTEM)
-      else
-        echo Not tagging "$FINAL_SYSTEM", no change from "$INITIAL_SYSTEM"
-      fi
+      echo Not tagging "$FINAL_SYSTEM", no change from "$INITIAL_SYSTEM"
     fi
   '';
 in pkgs.mkShell {
