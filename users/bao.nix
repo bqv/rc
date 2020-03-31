b7f9d0946c0d3cbb5195119e85ab5fbda2225bd8
Tony Olagbaiye <me@fron.io>
2020-03-31 09:04:50 +0100

    user: create ipfscat script

--- users/bao.nix
+++ users/bao.nix
@@ -1,6 +1,21 @@
 { config ? {}, pkgs, lib, ... }:
 
-{
+let
+  ipfscat = pkgs.writeShellScriptBin "ipfscat" ''
+    export IPFS_PATH='/var/lib/ipfs'
+    bold="$(${pkgs.ncurses}/bin/tput bold)"
+    sgr0="$(${pkgs.ncurses}/bin/tput sgr0)"
+    ${pkgs.ipfs}/bin/ipfs add $@ |\
+    ${pkgs.gnugrep}/bin/grep added |\
+    ${pkgs.coreutils}/bin/cut -d' ' -f 2 |\
+    ${pkgs.findutils}/bin/xargs -I{} echo "https://ipfs.io/ipfs/{}" |\
+    ${pkgs.coreutils}/bin/tee >( \
+    ${pkgs.xclip}/bin/xclip -i -r -selection primary ) >( \
+    ${pkgs.xclip}/bin/xclip -i -r -selection secondary ) >( \
+    ${pkgs.xclip}/bin/xclip -i -r -selection clipboard ) |\
+    ${pkgs.findutils}/bin/xargs echo $bold"Copied:"$sgr0
+  '';
+in {
   imports = [
     ../profiles/develop
   ];
@@ -39,8 +54,8 @@
       abduco dvtm git yadm vim htop pstree fortune cowsay coreutils pv # Shell Essential
       nmap wget curl # Networking
       gnupg bitwarden-cli protonvpn-cli-ng git-crypt # Security
-      file jq direnv # Utility
-      netsurf.browser # Utility
+      file jq direnv ipfscat # Utility
+      xsel xclip scrot # Utility
     ];
 
     home.file."mimeapps.list".force = lib.mkForce true;
