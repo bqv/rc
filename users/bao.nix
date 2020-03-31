a04892c2b146df443d0ad7f738b8801a28574703
Tony Olagbaiye <me@fron.io>
2020-03-31 09:04:50 +0100

    user: create ipfscat script

--- users/bao.nix
+++ users/bao.nix
@@ -1,6 +1,16 @@
 { config ? {}, pkgs, ... }:
 
-{
+let
+  ipfscat = pkgs.writeShellScriptBin "ipfscat" ''
+    export IPFS_PATH='/var/lib/ipfs'
+    ${pkgs.ipfs}/bin/ipfs add $@ |\
+    ${pkgs.gnugrep}/bin/grep added |\
+    ${pkgs.coreutils}/bin/cut -d' ' -f 2 |\
+    ${pkgs.findutils}/bin/xargs -I{} echo "https://ipfs.io/ipfs/{}" |\
+    ${pkgs.coreutils}/bin/tee /dev/stderr |\
+    ${pkgs.xsel}/bin/xsel -i
+  '';
+in {
   imports = [
     ../profiles/develop
   ];
@@ -39,8 +49,8 @@
       abduco dvtm git yadm vim htop pstree fortune cowsay coreutils pv # Shell Essential
       nmap wget curl # Networking
       gnupg bitwarden-cli protonvpn-cli-ng git-crypt # Security
-      file jq direnv # Utility
-      netsurf.browser # Utility
+      file jq direnv ipfscat # Utility
+      xsel xclip scrot # Utility
     ];
 
     xdg = let
