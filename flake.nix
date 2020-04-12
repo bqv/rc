426ff96220b69347532669c28a9a8ac5b3bf6935
Tony Olagbaiye <me@fron.io>
2020-04-12 01:44:21 +0100

    flake: clarify channels

--- flake.nix
+++ flake.nix
@@ -3,39 +3,51 @@
 
   epoch = 201909;
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/master";
-  inputs.staging.url = "github:nixos/nixpkgs/staging";
+  inputs.master.url = "github:nixos/nixpkgs/master";
+  inputs.staged.url = "github:nixos/nixpkgs/staging";
   inputs.small.url = "github:nixos/nixpkgs/nixos-unstable-small";
   inputs.large.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  inputs.dwarffs.url = "github:edolstra/dwarffs";
+  inputs.dwarffs.url = "github:edolstra/dwarffs/83c13981993fa54c4cac230f2eec7241ab8fd0a9";
   inputs.home.url = "github:rycee/home-manager/bqv-flakes";
   inputs.nur.url = "github:nix-community/NUR";
 
-  inputs.dwarffs.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.dwarffs.inputs.nixpkgs.follows = "master";
   inputs.home.inputs.nixpkgs.follows = "large";
   inputs.nur.inputs.nixpkgs.follows = "small";
 
   inputs.emacs = { url = "github:nix-community/emacs-overlay"; flake = false; };
 
-  outputs = inputs@{ self, home, nixpkgs, staging, small, large, dwarffs, nur, emacs }:
+  outputs = inputs@{ self, home, master, staged, small, large, dwarffs, nur, emacs }:
     let
       inherit (builtins) listToAttrs baseNameOf attrNames attrValues readDir;
-      inherit (nixpkgs.lib) fold recursiveUpdate setAttrByPath;
-      inherit (nixpkgs.lib) removeSuffix removePrefix splitString;
+      inherit (master.lib) fold recursiveUpdate setAttrByPath mapAttrs;
+      inherit (master.lib) removeSuffix removePrefix splitString;
       system = "x86_64-linux";
 
       pkgs = import large rec {
         inherit system;
         overlays = (attrValues self.overlays) ++ [
-          (self: super: { master = import nixpkgs { inherit config system overlays; }; })
-          (self: super: { staged = import staging { inherit config system overlays; }; })
-          (self: super: { small = import small { inherit config system overlays; }; })
-          (self: super: { large = import large { inherit config system overlays; }; })
+          (self: super: { master =
+             mapAttrs (k: v: builtins.trace "pkgs.${k} fetched from master" v)
+             (import master { inherit config system overlays; });
+           })
+          (self: super: { staged =
+             mapAttrs (k: v: builtins.trace "pkgs.${k} fetched from staging" v)
+             (import staged { inherit config system overlays; });
+           })
+          (self: super: { small =
+             mapAttrs (k: v: builtins.trace "pkgs.${k} fetched from nixos-unstable-small" v)
+             (import small { inherit config system overlays; });
+           })
+          (self: super: { large =
+             mapAttrs (k: v: builtins.trace "pkgs.${k} fetched from nixos-unstable" v)
+             (import large { inherit config system overlays; });
+           })
           (self: super: { pr = n: hash: import (super.fetchzip {
              name = "nixpkgs-pull-request-${toString n}";
              url = "https://github.com/NixOS/nixpkgs/archive/pull/${toString n}/head.zip";
-             hash = if hash == null then nixpkgs.lib.fakeSri else hash;
+             hash = if hash == null then master.lib.fakeSri else hash;
            }) { inherit config system overlays; }; })
           (import emacs)
         ];
@@ -45,6 +57,7 @@
     in {
       nixosConfigurations = import ./hosts (inputs // {
         inherit system pkgs;
+        nixpkgs = master;
       });
 
       overlay = import ./pkgs;
--- overlays/nftables-compat.nix
+++ overlays/nftables-compat.nix
@@ -1,3 +1,4 @@
 final: prev: {
   iptables = prev.iptables-nftables-compat;
+  systemd = prev.systemd.override { iptables = prev.iptables; };
 }
