d6eb53911df87b59ab4bc1581323f191de553c84
Tony Olagbaiye <me@fron.io>
2020-04-12 14:35:30 +0100

    delta: update network config for tethering

--- hosts/delta/network.nix
+++ hosts/delta/network.nix
@@ -18,14 +18,18 @@
     useDHCP = true;
     ipv4.addresses = [{ address = "192.168.0.254"; prefixLength = 24; }];
   };
-  networking.interfaces.enp5s0u1 = {
-    useDHCP = true;
-    ipv4.addresses = [{ address = "192.168.0.252"; prefixLength = 24; }];
-  };
   networking.interfaces.wlp3s0 = {
     useDHCP = true;
     ipv4.addresses = [{ address = "192.168.0.253"; prefixLength = 24; }];
   };
+
+  networking.interfaces.enp5s0u1 = {
+    useDHCP = true;
+    ipv4.addresses = [{ address = "192.168.0.252"; prefixLength = 24; }];
+  }; systemd.services.network-link-enp5s0u1.before = [];
+  networking.interfaces.enp0s20u3u1u2 = {
+    useDHCP = true;
+  }; systemd.services.network-link-enp0s20u3u1u2.before = [];
 
   networking.firewall.enable = false;
 
