6e993da188083e6762f44cd64364055bc58fcd52
Tony Olagbaiye <me@fron.io>
2020-04-16 01:47:12 +0100

    weechat: override weechat-matrix to actual git

--- profiles/weechat.nix
+++ profiles/weechat.nix
@@ -1,7 +1,26 @@
 { config, lib, pkgs, ... }:
 
 let
-  weechat-matrix = pkgs.weechatScripts.weechat-matrix;
+  matrix-nio = pkgs.python3Packages.matrix-nio.overrideAttrs (super: rec {
+    version = lib.substring 0 9 src.rev;
+    src = pkgs.fetchFromGitHub {
+      owner = "poljar";
+      repo = "matrix-nio";
+      rev = "98f0c244065ea59c2e5105bc0aab5811aea748cf";
+      hash = "sha256-GQPTnGxazR3WW8WGrC4X1oXvDXPMqQ5AZxdJns87C/Q=";
+    };
+  });
+  weechat-matrix = (pkgs.weechatScripts.weechat-matrix.override {
+    inherit matrix-nio;
+  }).overrideAttrs (super: rec {
+    version = lib.substring 0 9 src.rev;
+    src = pkgs.fetchFromGitHub {
+      owner = "poljar";
+      repo = "weechat-matrix";
+      rev = "d415841662549f096dda09390bfdebd3ca597bac";
+      hash = "sha256-QT3JNzIShaR8vlrWuGzBtLDHjn7Z6vhovcOAfttgUxo=";
+    };
+  });
   systemWeechat = pkgs.weechat.override {
     configure = { availablePlugins, ... }: {
       scripts = [ weechat-matrix ];
