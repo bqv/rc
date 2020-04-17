6e548c961f5a4a76636760c0797b7c2f6926c6e7
Tony Olagbaiye <me@fron.io>
2020-04-17 17:26:05 +0100

    weechat: move overrides to overlay

--- profiles/weechat.nix
+++ profiles/weechat.nix
@@ -1,38 +1,12 @@
 { config, lib, pkgs, ... }:
 
 let
-  matrix-nio = pkgs.python3Packages.matrix-nio.overrideAttrs (super: rec {
-    version = lib.substring 0 9 src.rev;
-    src = pkgs.fetchFromGitHub {
-      owner = "poljar";
-      repo = "matrix-nio";
-      rev = "98f0c244065ea59c2e5105bc0aab5811aea748cf";
-      hash = "sha256-GQPTnGxazR3WW8WGrC4X1oXvDXPMqQ5AZxdJns87C/Q=";
-    };
-    patches = [
-      (pkgs.fetchpatch {
-        url = "https://github.com/poljar/matrix-nio/commit/dfb2f9d12d557aff5bb07f01ac9691d8908bbf67.patch";
-        sha256 = "0x378cq71y15ri6g3fznfcg8ws4nrcfpaxcv38dzmlbizg08gwzg";
-      })
-    ];
-  });
-  weechat-matrix = (pkgs.weechatScripts.weechat-matrix.override {
-    inherit matrix-nio;
-  }).overrideAttrs (super: rec {
-    version = lib.substring 0 9 src.rev;
-    src = pkgs.fetchFromGitHub {
-      owner = "poljar";
-      repo = "weechat-matrix";
-      rev = "d415841662549f096dda09390bfdebd3ca597bac";
-      hash = "sha256-QT3JNzIShaR8vlrWuGzBtLDHjn7Z6vhovcOAfttgUxo=";
-    };
-  });
   systemWeechat = pkgs.weechat.override {
     configure = { availablePlugins, ... }: {
-      scripts = [ weechat-matrix ];
+      scripts = [ pkgs.weechatScripts.weechat-matrix ];
       plugins = builtins.attrValues (availablePlugins // {
         python = availablePlugins.python.withPackages (ps: with ps; [
-          weechat-matrix
+          pkgs.weechatScripts.weechat-matrix
           websocket_client
         ]);
       });
