5a09855ef9093ed769c35d7ecf6b8d1f7bf37b9a
Tony Olagbaiye <me@fron.io>
2020-04-18 19:43:38 +0100

    pleroma: promote wip from legacy

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -9,9 +9,13 @@ final: prev: {
 
   dejavu_nerdfont = prev.callPackage ./data/fonts/dejavu-nerdfont { };
 
+  matrix-appservice-irc = prev.callPackage ./servers/matrix-appservice-irc { };
+
   matrix-construct = prev.callPackage ./servers/matrix-construct { };
 
   mx-puppet-discord = prev.callPackage ./servers/mx-puppet-discord { };
+
+  pleroma = prev.callPackage ./servers/pleroma { };
 
   pure = prev.callPackage ./shells/zsh/pure { };
 }
