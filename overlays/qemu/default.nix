b613694dece8bf2e4df6c28ed9861c9dc27fc166
Tony Olagbaiye <me@fron.io>
2020-04-19 03:09:48 +0100

    qemu: promote from legacy

--- overlays/qemu/default.nix
+++ overlays/qemu/default.nix
@@ -1,11 +1,11 @@
-self: super:
+final: prev:
 
 {
-  qemu-user-arm = if self.stdenv.system == "x86_64-linux"
-    then self.pkgsi686Linux.callPackage ./qemu { user_arch = "arm"; }
-    else self.callPackage      ./qemu { user_arch = "arm"; };
-  qemu-user-x86 = self.callPackage ./qemu { user_arch = "x86_64"; };
-  qemu-user-arm64 = self.callPackage ./qemu { user_arch = "aarch64"; };
-  qemu-user-riscv32 = self.callPackage ./qemu { user_arch = "riscv32"; };
-  qemu-user-riscv64 = self.callPackage ./qemu { user_arch = "riscv64"; };
+  qemu-user-arm = if final.stdenv.system == "x86_64-linux"
+    then final.pkgsi686Linux.callPackage ./qemu { user_arch = "arm"; }
+    else final.callPackage      ./qemu { user_arch = "arm"; };
+  qemu-user-x86 = final.callPackage ./qemu { user_arch = "x86_64"; };
+  qemu-user-arm64 = final.callPackage ./qemu { user_arch = "aarch64"; };
+  qemu-user-riscv32 = final.callPackage ./qemu { user_arch = "riscv32"; };
+  qemu-user-riscv64 = final.callPackage ./qemu { user_arch = "riscv64"; };
 }
