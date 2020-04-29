6a8b154edfc2f78ac099b578443f52d4c2593b80
Tony Olagbaiye <me@fron.io>
2020-04-29 07:26:16 +0100

    hosts: fix image and NixOS

--- hosts/NixOS.nix
+++ hosts/NixOS.nix
@@ -1,6 +1,6 @@
 {
   ### root password is empty by default ###
-  imports = [ ../users/nixos ../users/root ];
+  imports = [ ../users/nixos.nix ../users/root.nix ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
