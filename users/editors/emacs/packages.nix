b901807f20e501a7b24f15b2ff895c4fc24bb0c7
Tony Olagbaiye <me@fron.io>
2020-05-22 18:21:09 +0100

    emacs: fix browse-url for >28

--- users/editors/emacs/packages.nix
+++ users/editors/emacs/packages.nix
@@ -6,7 +6,7 @@
     w3m findutils
     cmake gnumake gcc libtool libvterm gtk3 rls age rust-analyzer
   ] ++ (with emacsPackagesFor config.programs.emacs.package.unwrapped; [
-    use-package auto-compile gcmh diminish epkg log4e
+    use-package auto-compile gcmh diminish epkg log4e bug-hunter
 
     # usr-crit-bufmgmt.el
     persp-mode neotree persp-projectile window-purpose ivy-purpose treemacs treemacs-evil treemacs-magit treemacs-persp treemacs-projectile lsp-treemacs treemacs-icons-dired
