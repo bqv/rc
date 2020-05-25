5a825248782e9bb1da6408d59a29d6ea2bf4eaa0
Tony Olagbaiye <bqv@fron.io>
2020-05-25 01:51:26 +0100

    phi: fix build

--- overlays/nftables-compat.nix
+++ overlays/nftables-compat.nix
@@ -1,20 +1,12 @@
-final: prev: {
+final: prev: rec {
   iptables = prev.iptables-nftables-compat;
   systemd = prev.systemd.override { iptables = prev.iptables; };
-  webkitgtk = prev.webkitgtk.override {
-    geoclue2 = prev.geoclue2.override {
-      glib-networking = prev.glib-networking.override {
-        libproxy = prev.libproxy.override {
-          networkmanager = prev.networkmanager.override {
-            dhcp = prev.dhcp.override {
-              iproute = prev.iproute.override {
-                iptables = prev.iptables;
-              };
-            };
-            iptables = prev.iptables;
-          };
-        };
+  networkmanager = prev.networkmanager.override {
+    dhcp = prev.dhcp.override {
+      iproute = prev.iproute.override {
+        iptables = prev.iptables;
       };
     };
+    iptables = prev.iptables;
   };
 }
