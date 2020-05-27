1a37654a79548970f6316f7541824d95a2bf42d6
Tony Olagbaiye <me@fron.io>
2020-05-27 17:29:08 +0100

    bao: add tree

--- users/bao.nix
+++ users/bao.nix
@@ -98,7 +98,7 @@
       pv pup # Pipe Management
       nmap wget curl ipfscat mitmproxy # Network Utilities
       bitwarden-cli protonvpn-cli-ng # Password Management
-      file exa unrar unzip ncdu # File Management
+      file exa unrar unzip ncdu tree # File Management
       xpra xsel xclip scrot # X11 Utilities
       gdb lldb radare2 radare2-cutter # Debug Utilities
     ] ++ lib.optional home-config.programs.emacs.enable emms-play-file;
