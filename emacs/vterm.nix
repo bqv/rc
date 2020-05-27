114dff104956833f51b0334b02774850b105c0b4
Tony Olagbaiye <me@fron.io>
2020-05-27 20:46:39 +0100

    emacs: fix init errors

--- emacs/vterm.nix
+++ emacs/vterm.nix
@@ -8,7 +8,6 @@
       ;; banish ansi-term :)
       ;(defalias 'ansi-term (lambda (&rest _) (call-interactively #'vterm)))
       (setq vterm-shell "${pkgs.xonsh.pname}")
-      (use-package vterm-toggle :demand t)
     '';
     systemDeps = with pkgs; [ cmake libtool ];
   };
