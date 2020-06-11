934095e50a5659dd1c50b17a02a6cf42df9e5679
Tony Olagbaiye <me@fron.io>
2020-06-11 05:46:13 +0100

    emacs: add dir-locals

--- emacs/default.nix
+++ emacs/default.nix
@@ -2,5 +2,7 @@
 
 {
   imports = lib.filter (p: lib.hasSuffix ".nix" p && builtins.baseNameOf p != "default.nix")
-    (lib.mapAttrsToList (file: _: ./. + "/${file}") (builtins.readDir ./.));
+    (lib.mapAttrsToList (file: _: ./. + "/${file}")  # strings to paths
+      (lib.filterAttrs (f: _: f != ".dir-locals.el") # remove bad files
+        (builtins.readDir ./.)));                    # enumerate folder
 }
