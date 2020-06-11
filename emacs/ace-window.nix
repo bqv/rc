fd31730d92ec12249bce43967aa65ceb4dbcaed5
Tony Olagbaiye <me@fron.io>
2020-06-11 06:12:54 +0100

    emacs: use ace-window instead of switch-window
    switch-window doesn't even work... ace does

--- emacs/ace-window.nix
+++ emacs/ace-window.nix
@@ -5,11 +5,17 @@
     enable = false;
     demand = true;
     bind = {
-      "M-m" = "ace-window";
+      "M-m" = "ace-window"; # deprecating
+      "s-m" = "ace-window"; # usurped from switch-window
     };
     config = ''
       (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)
-            aw-dispatch-always t)
+            aw-dispatch-always t
+            aw-scope 'global
+            aw-minibuffer-flag t)
+      (ace-window-display-mode)
+      (exwm-input-set-key (kbd "s-m") 'ace-window)
+      (exwm-input--update-global-prefix-keys)
     '';
   };
 }
