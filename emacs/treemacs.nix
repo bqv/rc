ed0423f78d245c5e8b5182b1b2572c23ffbf5a39
Tony Olagbaiye <me@fron.io>
2020-06-28 18:00:04 +0100

    emacs::treemacs: bind f9

--- emacs/treemacs.nix
+++ emacs/treemacs.nix
@@ -3,6 +3,9 @@
 {
   emacs-loader.treemacs = {
     demand = true;
+    bind = {
+      "<f9>" = "treemacs";
+    };
     config = ''
       (require 'electric)
       (defun electric-indent-local-mode (&rest r) nil)
