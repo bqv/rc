816b56c490e084d9aa7c72b95b55e81ba86dd054
Tony Olagbaiye <bqv@fron.io>
2020-06-29 07:21:56 +0100

    fixup! zeta::anki: init ankisyncd container

--- overlays/ankisyncd.nix
+++ overlays/ankisyncd.nix
@@ -1,6 +1,6 @@
 final: prev: {
   ankisyncd = prev.ankisyncd.overrideAttrs (_: {
-    src = fetchFromGitHub {
+    src = final.fetchFromGitHub {
       owner = "ankicommunity";
       repo = "anki-sync-server";
       rev = "125f7bb1b58ccdeeaa710d4500844b7a4cfae85c";
