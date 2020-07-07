59e5c75431c3e2efa4d04b0214c35a7292916092
Tony Olagbaiye <me@fron.io>
2020-07-07 20:41:22 +0100

    pkgs::gccEmacs: merge 'feature/native-comp' with 'masm11/pgtk'

--- overlays/gccemacs.nix
+++ overlays/gccemacs.nix
@@ -1,5 +1,16 @@
 final: prev: rec {
-  gccEmacs = final.emacsGcc;
+  gccEmacs = final.emacsGcc.overrideAttrs (old: rec {
+    name = "${old.name}-${version}";
+    version = prev.lib.substring 0 7 src.rev;
+    src = prev.fetchFromGitHub {
+      owner = "bqv";
+      repo = "emacs";
+      rev = "0f468a2f8bd6b8950be92431905b79f4d36ef8fd";
+      sha256 = "09hkn99jdwa0wm3kl1lfdirby22pyd5qa42hqamfd5x70s67141v";
+    };
+    buildInputs = old.buildInputs ++ [ prev.cairo ];
+    configureFlags = old.configureFlags ++ [ "--with-pgtk" "--with-cairo" "--with-modules" ];
+  });
 
   gccEmacsPackages = prev.lib.dontRecurseIntoAttrs (final.emacsPackagesFor gccEmacs);
 
