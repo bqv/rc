4f16112cf8b543b34e04c9da43c4e11d28b3d6c1
Tony Olagbaiye <me@fron.io>
2020-07-08 05:58:14 +0100

    overlays::swc:: debug

--- profiles/services/wayland/swc-servers/velox.nix
+++ profiles/services/wayland/swc-servers/velox.nix
@@ -21,7 +21,17 @@ in {
 
   ###### implementation
   config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ velox ];
+
     services.swc-launch.server = { available_servers = [ "velox" ]; };
     services.swc-launch.server.velox = { command = "${pkgs.velox}/bin/velox"; };
+
+   #services.swc-launch.server.velox = { command = "${config.security.wrapperDir}/velox"; };
+   #security.wrappers.velox = {
+   #  source = "${pkgs.velox}/bin/velox";
+   #  owner = "root";
+   #  group = "root";
+   #  setuid = true;
+   #};
   };
 }
