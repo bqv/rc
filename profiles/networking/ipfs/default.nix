88f138d6c887216fb69f9097e6e9afa4e3905d7e
Tony Olagbaiye <me@fron.io>
2020-07-14 12:22:48 +0100

    profiles::ipfs: dirty patches for a broken module

--- profiles/networking/ipfs/default.nix
+++ profiles/networking/ipfs/default.nix
@@ -17,4 +17,10 @@
     };
     gatewayAddress = "/ip4/127.0.0.1/tcp/4501";
   };
+
+  systemd.services.ipfs-init = builtins.trace "ipfs still running ephemerally" {
+    path = [ pkgs.coreutils ];
+    serviceConfig.ExecStartPre = "rm -rf /var/lib/ipfs";
+    serviceConfig.ExecStartPost = "chmod g+r /var/lib/ipfs/config";
+  };
 }
