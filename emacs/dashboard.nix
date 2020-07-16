b59e30e3495fc3b9ec8b74e58ee379e87596a1d6
Tony Olagbaiye <me@fron.io>
2020-07-17 00:20:38 +0100

    emacs::dashboard: bugged out

--- emacs/dashboard.nix
+++ emacs/dashboard.nix
@@ -7,7 +7,7 @@
       (dashboard-setup-startup-hook)
       (setq dashboard-center-content t)
       (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
-      (setq dashboard-items '((recents  . 5)
+      (setq dashboard-items '(;(recents  . 5)
                               (bookmarks . 5)
                               (projects . 5)
                               (agenda . 5)))
