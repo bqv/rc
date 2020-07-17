fa9d8ef39be619f1f6172bbb401e13c679930eb2
Tony Olagbaiye <me@fron.io>
2020-07-17 04:08:08 +0100

    flake::lock: upgrade wayland and use pgtk

--- overlays/gccemacs.nix
+++ overlays/gccemacs.nix
@@ -7,16 +7,19 @@ final: prev: rec {
       rev = "0f468a2f8bd6b8950be92431905b79f4d36ef8fd";
       sha256 = "09hkn99jdwa0wm3kl1lfdirby22pyd5qa42hqamfd5x70s67141v";
     };
-    nrev = "2593bbee51f4d15d3a4fc1d4e2e3b215222f783a";
-    wrev = "54ef44216cef3480d69b4da2f46b8db5b0249d64";
+
+    nrev = "2593bbee51f4d15d3a4fc1d4e2e3b215222f783a";#final.emacsGcc.src.rev;
+    wrev = final.emacs-pgtk.src.rev;
+
     nativecomp = prev.fetchzip {
       url = "http://github.com/emacs-mirror/emacs/archive/${nrev}.zip";
-      sha256 = "sha256-3jLpUATsIupns9uTLDiRI84vX21Crps/TQquhUOMUkQ=";
+      sha256 = "3jLpUATsIupns9uTLDiRI84vX21Crps/TQquhUOMUkQ=";#final.emacsGcc.src.outputHash;
     };
     pgtk = prev.fetchzip {
       url = "http://github.com/masm11/emacs/archive/${wrev}.zip";
-      sha256 = "sha256-Ja9Y5eJ5c6D29GCLakZOOGde55i3WB96KVyMlgXr0xE=";
+      sha256 = final.emacs-pgtk.src.outputHash;
     };
+
     version = "${shortrev nrev}+${shortrev wrev}";
     src = prev.runCommandNoCC "git-merge" { buildInputs = with prev; [ git ]; } ''
       cp ${pgtk} src -Lr && cd src && chmod -R a+w .
