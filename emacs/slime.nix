8ec538201a4a791c52ae0be9c9d30fbde7274687
Tony Olagbaiye <me@fron.io>
2020-07-18 08:01:25 +0100

    emacs::slime: nyxt clipboard-ring

--- emacs/slime.nix
+++ emacs/slime.nix
@@ -6,6 +6,17 @@
     config = ''
       (setq slime-auto-connect 'always)
       (setq slime-enable-evaluate-in-emacs t)
+
+      (defun nyxt-copy (text)
+        (interactive "s")
+        (slime-eval `(containers:insert-item (nyxt:clipboard-ring nyxt::*browser*) ,text)))
+
+      (defun nyxt-paste ()
+        (interactive)
+        (unless (slime-eval '(containers:empty-p (nyxt:clipboard-ring nyxt-user::*browser*)))
+          (let ((s (slime-eval '(containers:first-item (nyxt:clipboard-ring nyxt-user::*browser*)))))
+            (when s
+              (kill-new s)))))
     '';
   };
 }
