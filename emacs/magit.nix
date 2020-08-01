2307857c0caabbb8bb06eaf5f2dee635ea20484c
Tony Olagbaiye <me@fron.io>
2020-08-01 19:11:00 +0100

    emacs::magit: use magit-wip mode

--- emacs/magit.nix
+++ emacs/magit.nix
@@ -11,6 +11,8 @@
       (setq magit-log-show-refname-after-summary t)
       (setq magit-clone-default-directory (expand-file-name "~/dev/"))
       (setq ediff-window-setup-function 'ediff-setup-windows-plain)
+      (setq magit-wip-merge-branch t)
+      (magit-wip-mode)
     '';
     systemDeps = with pkgs; [ git ];
   };
