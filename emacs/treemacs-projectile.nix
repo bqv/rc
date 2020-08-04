3ee643b65757374cc2497cec0f97ecbc8143eea0
Tony Olagbaiye <me@fron.io>
2020-08-04 20:19:48 +0100

    emacs::treemacs-projectile: hook to projectile

--- emacs/treemacs-projectile.nix
+++ emacs/treemacs-projectile.nix
@@ -4,5 +4,12 @@
   emacs-loader.treemacs-projectile = {
     demand = true;
     after = [ "treemacs" "projectile" ];
+    config = ''
+      (defun bqv/treemacs-open ()
+        (unless (equalp (treemacs-current-visibility) 'visible)
+          (with-selected-window (selected-window)
+            (treemacs))))
+      (add-hook 'projectile-after-switch-project-hook 'bqv/treemacs-open)
+    '';
   };
 }
