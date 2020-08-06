763a33c3a53cfabc37078c007d7a97077463a64c
Tony Olagbaiye <me@fron.io>
2020-08-06 21:35:39 +0100

    overlays::xonsh: patch prompt-toolkit

--- overlays/xonsh.nix
+++ overlays/xonsh.nix
@@ -1,9 +1,28 @@
-final: prev: with final.xontribs; rec {
+final: prev: let
+  pythonOverrides = self: super: {
+    prompt_toolkit = super.prompt_toolkit.overridePythonAttrs (_: {
+      src = final.fetchFromGitHub {
+        owner = "bobhy";
+        repo = "python-prompt-toolkit";
+        rev = "th-threadsafe-load-2";
+        sha256 = "sha256-P6/r07bifrib0Pti83d2q5cKYr1q6jxRpT44539kctE=";
+      };
+    });
+  };
+  python = final.python3.override {
+    self = python;
+    packageOverrides = pythonOverrides;
+  };
+in with final.xontribs; rec {
   xonsh = prev.xonsh.overridePythonAttrs (o: rec {
     doCheck = false;
-    propagatedBuildInputs = o.propagatedBuildInputs ++ [
-      final.python3Packages.nixpkgs
-      final.python3Packages.pip
+    propagatedBuildInputs = [
+      python.pkgs.ply
+      python.pkgs.prompt_toolkit
+      python.pkgs.pygments
+
+      python.pkgs.nixpkgs
+      python.pkgs.pip
       apt-tabcomplete
       autoxsh
       avox
