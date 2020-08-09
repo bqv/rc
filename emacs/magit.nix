026dcae2e1255193f3a9220fa648193e7cbe7f4f
Tony Olagbaiye <me@fron.io>
2020-08-09 17:05:23 +0100

    emacs::magit: new binding

--- emacs/magit.nix
+++ emacs/magit.nix
@@ -6,11 +6,13 @@
     bind = {
       "C-x g" = "magit-status";
       "C-x M-g" = "magit-dispatch-popup";
+      "C-c g" = "magit-dispatch";
     };
     config = ''
       (setq magit-log-show-refname-after-summary t)
       (setq magit-clone-default-directory (expand-file-name "~/dev/"))
       (setq ediff-window-setup-function 'ediff-setup-windows-plain)
+      (setq magit-section-initial-visibility-alist nil)
       (setq magit-wip-merge-branch t)
       (magit-wip-mode)
     '';
