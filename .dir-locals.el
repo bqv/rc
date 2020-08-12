790c98e1ef466a1369f810f5218db452bc6d5d3f
Tony Olagbaiye <me@fron.io>
2020-08-12 04:14:24 +0100

    profiles::ipfs: move fs

--- .dir-locals.el
+++ .dir-locals.el
@@ -6,5 +6,5 @@
         ;             (locate-dominating-file buffer-file-name ".dir-locals.el")))
          (compile-command . "systemd-run --user --scope nix run . -- -Tbasv")
          (eval . (setq projectile-project-compilation-cmd
-                       "systemd-run --user -u nixos --scope foreground nix build '' ./result"))
+                       "systemd-run --user -u nixos --scope if nix build '' ./result"))
          )))
