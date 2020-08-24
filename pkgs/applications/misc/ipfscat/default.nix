7d3e82cf650685bd7b9963571f9b672756616f6b
Tony Olagbaiye <me@fron.io>
2020-08-24 19:23:56 +0100

    pkgs::ipfscat: dont pin stuff by default

--- pkgs/applications/misc/ipfscat/default.nix
+++ pkgs/applications/misc/ipfscat/default.nix
@@ -1,20 +1,22 @@
 { config, pkgs, lib, ... }:
 
-pkgs.writeShellScriptBin "ipfscat" ''
+let
+  gateway = "https://gateway.ipfs.io";
+in pkgs.writeShellScriptBin "ipfscat" ''
   export IPFS_PATH='/var/lib/ipfs'
   bold="$(${pkgs.ncurses}/bin/tput bold)"
   sgr0="$(${pkgs.ncurses}/bin/tput sgr0)"
   if [ -z "$DISPLAY" ]; then
-  ${pkgs.ipfs}/bin/ipfs add $@ |\
+  ${pkgs.ipfs}/bin/ipfs add --pin=false $@ |\
   ${pkgs.gnugrep}/bin/grep added |\
   ${pkgs.coreutils}/bin/cut -d' ' -f 2 |\
-  ${pkgs.findutils}/bin/xargs -I{} echo "https://gateway.ipfs.io/ipfs/{}" |\
+  ${pkgs.findutils}/bin/xargs -I{} echo "${gateway}/ipfs/{}" |\
   ${pkgs.findutils}/bin/xargs echo $bold"Copy:"$sgr0
   else
-  ${pkgs.ipfs}/bin/ipfs add $@ |\
+  ${pkgs.ipfs}/bin/ipfs add --pin=false $@ |\
   ${pkgs.gnugrep}/bin/grep added |\
   ${pkgs.coreutils}/bin/cut -d' ' -f 2 |\
-  ${pkgs.findutils}/bin/xargs -I{} echo "https://gateway.ipfs.io/ipfs/{}" |\
+  ${pkgs.findutils}/bin/xargs -I{} echo "${gateway}/ipfs/{}" |\
   ${pkgs.xclip}/bin/xclip -i -r -f -selection primary |\
   ${pkgs.xclip}/bin/xclip -i -r -f -selection secondary |\
   ${pkgs.xclip}/bin/xclip -i -r -f -selection clipboard |\
