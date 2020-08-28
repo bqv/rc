06676dc18240289f5de3575022e40fcd6ac5ba91
Tony Olagbaiye <me@fron.io>
2020-08-28 22:46:55 +0100

    hosts::zeta: add vervis container

--- hosts/zeta/default.nix
+++ hosts/zeta/default.nix
@@ -2,15 +2,17 @@
 
 {
   imports = [
-    ../../legacy/containers/secure.nix    # 10.1.0.x
-    ../../legacy/containers/sandbox.nix   # 10.2.0.x
-   #../../legacy/containers/certmon.nix   # 10.3.0.x
-    ../../legacy/containers/authority.nix # 10.4.0.x
-    ../../legacy/containers/search.nix    # 10.5.0.x
-    ../../legacy/containers/mastodon.nix  # 10.6.0.x
-    ../../legacy/containers/matrix.nix    # 10.7.0.x
-    ../../legacy/containers/hydroxide.nix # 10.8.0.x
-    ../../legacy/containers/anki.nix      # 10.9.0.x
+    ../../legacy/containers/secure.nix    # 10. 1.0.x
+    ../../legacy/containers/sandbox.nix   # 10. 2.0.x
+   #../../legacy/containers/certmon.nix   # 10. 3.0.x
+    ../../legacy/containers/authority.nix # 10. 4.0.x
+    ../../legacy/containers/search.nix    # 10. 5.0.x
+    ../../legacy/containers/mastodon.nix  # 10. 6.0.x
+    ../../legacy/containers/matrix.nix    # 10. 7.0.x
+    ../../legacy/containers/hydroxide.nix # 10. 8.0.x
+    ../../legacy/containers/anki.nix      # 10. 9.0.x
+    ../../legacy/containers/vervis.nix    # 10.10.0.x
+   #../../legacy/containers/lemmy.nix     # 10.11.0.x
     ../../profiles/meta/fatal-warnings.nix
     ../../profiles/misc/qemu.nix
     ../../profiles/security/sudo.nix
@@ -150,6 +152,7 @@
       host  ${mastodon.name} ${mastodon.user} 10.6.0.0/24 trust
       host  matrix-synapse matrix-synapse 10.7.0.0/24 trust
       host  mx-puppet-discord mx-puppet-discord 10.7.0.0/24 trust
+      host  vervis vervis 10.10.0.0/24 trust
       host  lemmy lemmy ${hosts.wireguard.delta}/32 trust
      '');
   services.postgresql.ensureUsers = [
