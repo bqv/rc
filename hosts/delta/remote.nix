a6f73e686036016dfd21411d67f90fc2f3387437
Tony Olagbaiye <me@fron.io>
2020-09-24 20:19:51 +0100

    hosts::delta: allowforward X11

--- hosts/delta/remote.nix
+++ hosts/delta/remote.nix
@@ -28,6 +28,7 @@
       { path = "/etc/ssh/ssh_host_ed25519_key"; type = "ed25519"; }
     ];
     permitRootLogin = "without-password";
+    forwardX11 = true;
     extraConfig = ''
       HostCertificate /etc/ssh/ssh_host_rsa_key-cert.pub
       HostCertificate /etc/ssh/ssh_host_ecdsa_key-cert.pub
