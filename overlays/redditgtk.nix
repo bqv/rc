f24a3e511d128273c3df50472f031a0027ad1c83
Tony Olagbaiye <me@fron.io>
2020-09-25 17:10:27 +0100

    overlays::redditgtk: update redditgtk

--- overlays/redditgtk.nix
+++ overlays/redditgtk.nix
@@ -13,7 +13,12 @@ in { redditgtk =
   stdenv.mkDerivation rec {
     pname = "redditgtk";
     version = lib.substring 0 7 src.rev;
-    src = fetchGit { url = "https://gitlab.gnome.org/GabMus/redditgtk"; rev = "c7406bc5fc5927d5b7c461698af2e69635c13542"; };
+    src = fetchgit {
+      url = "https://gitlab.gnome.org/GabMus/redditgtk";
+      rev = "3afe3a7fc1d3ed81adb28c7c94ab24401aba0255";
+      sha256 = "1yqxj006iwwpfva612ni602zzhqmj8z9y8ivgq3z56cr31xc2h49";
+      # date = 2020-09-25T17:13:01+02:00;
+    };
     nativeBuildInputs = [ meson cmake pkg-config ninja python3Packages.wrapPython wrapGAppsHook ];
     buildInputs = [ glib gobject-introspection gtk3 webkitgtk gtksourceview4 libhandy ];
     pythonPath = with python3Packages; [ pygobject3 praw flask dateutil ];
