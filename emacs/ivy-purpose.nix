2fe2deee9045c43a247ec861c4a916db1a89e9b9
Tony Olagbaiye <me@fron.io>
2020-10-20 23:01:52 +0100

    emacs::purpose: stop using bufler again, it's slow af

--- emacs/ivy-purpose.nix
+++ emacs/ivy-purpose.nix
@@ -8,7 +8,7 @@
       (ivy-purpose-setup)
       ;(define-key purpose-mode-map "C-x b" nil)
       (define-purpose-prefix-overload purpose-switch-buffer-overload
-        '(bufler-switch-buffer ;ivy-purpose-switch-buffer-without-purpose
+        '(ivy-purpose-switch-buffer-without-purpose ;bufler-switch-buffer
           ivy-switch-buffer
           ivy-purpose-switch-buffer-with-some-purpose))
     '';
