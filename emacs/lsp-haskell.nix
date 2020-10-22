c7ba6d58e06c2fe7a531ed676271912a92a31317
Tony Olagbaiye <me@fron.io>
2020-10-22 23:33:48 +0100

    emacs::lsp-haskell: make work

--- emacs/lsp-haskell.nix
+++ emacs/lsp-haskell.nix
@@ -8,8 +8,11 @@
       { haskell-mode-hook = "lsp"; }
     ];
     config = ''
+      (add-to-list 'haskell-mode-hook 'lsp)
+      (setq lsp-haskell-server-path "${pkgs.haskellPackages.ghcide}/bin/ghcide")
+      (setq lsp-haskell-server-args nil)
       (setq lsp-haskell-process-path-hie "ghcide")
-      (setq lsp-haskell-process-args-hie '())
+      (setq lsp-haskell-process-args-hie nil)
     '';
   };
 }
