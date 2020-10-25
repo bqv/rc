a50610cc2591a3239317b4993a4e189061e3f51a
Tony Olagbaiye <me@fron.io>
2020-10-25 04:51:26 +0000

    emacs::lsp-haskell: add to list after init

--- emacs/lsp-haskell.nix
+++ emacs/lsp-haskell.nix
@@ -4,11 +4,11 @@
   emacs-loader.lsp-haskell = {
     demand = true;
     after = [ "lsp" ];
-    hook = [
-      { haskell-mode-hook = "lsp"; }
-    ];
+   #hook = [
+   #  { haskell-mode-hook = "lsp"; }
+   #];
     config = ''
-      (add-to-list 'haskell-mode-hook 'lsp)
+      (add-to-list 'after-init-hook (lambda (&rest r) (add-to-list 'haskell-mode-hook 'lsp)))
       (setq lsp-haskell-server-path "${pkgs.haskellPackages.ghcide}/bin/ghcide")
       (setq lsp-haskell-server-args nil)
       (setq lsp-haskell-process-path-hie "ghcide")
