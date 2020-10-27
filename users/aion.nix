10c3d96cb29ca689d5828a8b99693a460dc5353b
Tony Olagbaiye <me@fron.io>
2020-10-27 21:43:57 +0000

    users::aion: many amendments

--- users/aion.nix
+++ users/aion.nix
@@ -20,19 +20,18 @@
     # If not running interactively, don't do anything
     [[ $- != *i* ]] && return
 
-    source /etc/profile
+    . /etc/profile
+    . $HOME/.bash_profile
 
-    export LOCALE_ARCHIVE=${pkgs.glibcLocales}/lib/locale/locale-archive
+    [[ -x "$HOME/.nix-profile/bin/fish" ]] && exec $HOME/.nix-profile/bin/fish
 
     PS1='[\u@\h \W]\$ '
-
-    PS1="\n\[\e[1;30m\][''$$:$PPID - \j:\!\[\e[1;30m\]]\[\e[0;36m\] \T \[\e[1;30m\][\[\e[1;34m\]\u@\H\[\e[1;30m\]:\[\e[0;37m\]''${SSH_TTY:-o} \[\e[0;32m\]+''${SHLVL}\[\e[1;30m\]] \[\e[1;37m\]\w\[\e[0;37m\] \n\$ "
-  '';
-  home.file.".profile".text = ''
   '';
   home.file.".bash_profile".text = ''
-    export LOCALE_ARCHIVE=${pkgs.glibcLocales}/lib/locale/locale-archive
-    [[ $INSIDE_EMACS == "vterm" ]] && [[ $IN_NIX_SHELL == "" ]] && exec fish
+    . $HOME/.nix-profile/etc/profile.d/nix.sh
+
+    [[ $- == *i* ]] && \
+    [[ -x "$HOME/.nix-profile/bin/fish" ]] && exec $HOME/.nix-profile/bin/fish
   '';
   home.file.".config/nixpkgs/config.nix".text = ''
     {
@@ -62,103 +61,110 @@
   services.unclutter.extraOptions = [ "ignore-scrolling" ];
   services.sxhkd.enable = true;
   services.sxhkd.extraPath = "/bin:/usr/bin:/usr/local/bin:/snap/bin";
-  services.sxhkd.keybindings = {
+  services.sxhkd.keybindings = let
+    mod = "super";
+  in {
     ## wm independent hotkeys
 
     # terminal emulator
-    "mode_switch + {_,shift + }Return" = "stterm -f 'DejaVu Sans Mono-9'{_, -- mosh bao@delta -- emacsclient -t}";
+    "${mod} + {_,shift + }Return" = "st -f 'DejaVu Sans Mono-9' {env -u TMUX_TMPDIR $SHELL, -- mosh bao@delta -- emacsclient -t}";
 
     # program launcher
-    "mode_switch + @space" = "rofi -show run";
+    "${mod} + @space" = "rofi -show run";
 
     # make sxhkd reload its configuration files:
-    "mode_switch + Escape" = "pkill -USR1 -x sxhkd";
+    "${mod} + Escape" = "pkill -USR1 -x sxhkd";
 
     ## bspwm hotkeys
 
     # quit bspwm normally
-    "mode_switch + alt + Escape" = "bspc quit";
+    "${mod} + alt + Escape" = "bspc quit";
 
     # close and kill
-    "mode_switch + {_,shift + }w" = "bspc node -{c,k}";
+    "${mod} + {_,shift + }w" = "bspc node -{c,k}";
 
     # alternate between the tiled and monocle layout
-    "mode_switch + m" = "bspc desktop -l next";
+    "${mod} + m" = "bspc desktop -l next";
 
     # if the current node is automatic, send it to the last manual, otherwise pull the last leaf
-    "mode_switch + y" = "bspc query -N -n focused.automatic && bspc node -n last.!automatic || bspc node last.leaf -n focused";
+    "${mod} + y" = "bspc query -N -n focused.automatic && bspc node -n last.!automatic || bspc node last.leaf -n focused";
 
     # swap the current node and the biggest node
-    "mode_switch + g" = "bspc node -s biggest";
+    "${mod} + g" = "bspc node -s biggest";
 
     ## state/flags
 
     # set the window state
-    "mode_switch + {t,shift + t,s,f}" = "bspc node -t {tiled,pseudo_tiled,floating,fullscreen}";
+    "${mod} + {t,shift + t,s,f}" = "bspc node -t {tiled,pseudo_tiled,floating,fullscreen}";
 
     # set the node flags
-    "mode_switch + ctrl + {x,y,z}" = "bspc node -g {locked,sticky,private}";
+    "${mod} + ctrl + {x,y,z}" = "bspc node -g {locked,sticky,private}";
 
     ## focus/swap
 
     # focus the node in the given direction
-    "mode_switch + {_,shift + }{h,j,k,l}" = "bspc node -{f,s} {west,south,north,east}";
+    "${mod} + {_,shift + }{h,j,k,l}" = "bspc node -{f,s} {west,south,north,east}";
 
     # focus the node for the given path jump
-    "mode_switch + {p,b,comma,period}" = "bspc node -f @{parent,brother,first,second}";
+    "${mod} + {p,b,comma,period}" = "bspc node -f @{parent,brother,first,second}";
 
     # focus the next/previous node in the current desktop
-    "mode_switch + {_,shift + }c" = "bspc node -f {next,prev}.local";
+    "${mod} + {_,shift + }c" = "bspc node -f {next,prev}.local";
 
     # focus the next/previous desktop in the current monitor
-    "mode_switch + bracket{left,right}" = "bspc desktop -f {prev,next}.local";
+    "${mod} + bracket{left,right}" = "bspc desktop -f {prev,next}.local";
 
     # focus the last node/desktop
-    "mode_switch + {grave,Tab}" = "bspc {node,desktop} -f last";
+    "${mod} + {grave,Tab}" = "bspc {node,desktop} -f last";
 
     # focus the older or newer node in the focus history
-    "mode_switch + {o,i}" = ''bspc wm -h off; \
+    "${mod} + {o,i}" = ''bspc wm -h off; \
         bspc node {older,newer} -f; \
         bspc wm -h on'';
 
     # focus or send to the given desktop
-    "mode_switch + {_,shift + }{1-9,0}" = "bspc {desktop -f,node -d} '^{1-9,10}'";
+    "${mod} + {_,shift + }{1-9,0}" = "bspc {desktop -f,node -d} '^{1-9,10}'";
 
     ## preselect
 
     # preselect the direction
-    "mode_switch + ctrl + {h,j,k,l}" = "bspc node -p {west,south,north,east}";
+    "${mod} + ctrl + {h,j,k,l}" = "bspc node -p {west,south,north,east}";
 
     # preselect the ratio
-    "mode_switch + ctrl + {1-9}" = "bspc node -o 0.{1-9}";
+    "${mod} + ctrl + {1-9}" = "bspc node -o 0.{1-9}";
 
     # cancel the preselection for the focused node
-    "mode_switch + ctrl + space" = "bspc node -p cancel";
+    "${mod} + ctrl + space" = "bspc node -p cancel";
 
     # cancel the preselection for the focused desktop
-    "mode_switch + ctrl + shift + space" = "bspc query -N -d | xargs -I id -n 1 bspc node id -p cancel";
+    "${mod} + ctrl + shift + space" = "bspc query -N -d | xargs -I id -n 1 bspc node id -p cancel";
 
     ## move/resize
 
     # expand a window by moving one of its side outward
-    "mode_switch + alt + {h,j,k,l}" = "bspc node -z {left -20 0,bottom 0 20,top 0 -20,right 20 0}";
+    "${mod} + alt + {h,j,k,l}" = "bspc node -z {left -20 0,bottom 0 20,top 0 -20,right 20 0}";
 
     # contract a window by moving one of its side inward
-    "mode_switch + alt + shift + {h,j,k,l}" = "bspc node -z {right -20 0,top 0 20,bottom 0 -20,left 20 0}";
+    "${mod} + alt + shift + {h,j,k,l}" = "bspc node -z {right -20 0,top 0 20,bottom 0 -20,left 20 0}";
 
     # move a floating window
-    "mode_switch + {Left,Down,Up,Right}" = "bspc node -v {-20 0,0 20,0 -20,20 0}";
+    "${mod} + {Left,Down,Up,Right}" = "bspc node -v {-20 0,0 20,0 -20,20 0}";
   };
 
   #systemd.user.startServices = true; # broken by the [nix-env -> nix profile] move
 
   xsession.windowManager.bspwm.enable = true;
   xdg.configFile."bspwm/bspwmrc".executable = true;
-  xdg.configFile."bspwm/bspwmrc".text = ''
+  xdg.configFile."bspwm/bspwmrc".text = let
+    compiledLayout = with pkgs.xorg; pkgs.runCommand "xkb-layout" {} ''
+      ${xkbcomp}/bin/xkbcomp -I${./data/xkb} ${./data/xkb/keymap/cb} $out
+    '';
+  in ''
     #!/bin/sh
 
-    systemctl --user start sxhkd
-    systemctl --user start unclutter
+    xkbcomp ${compiledLayout} $DISPLAY
+    sxhkd &
+    unclutter &
 
     bspc monitor -d I II III IV V
 
@@ -170,19 +176,10 @@
     bspc config gapless_monocle      true
 
     bspc rule -a Gimp desktop='^8' state=floating follow=on
-    bspc rule -a Chromium desktop='^2'
+    bspc rule -a qutebrowser desktop='^2'
     bspc rule -a mplayer2 state=floating
     bspc rule -a Kupfer.py focus=on
     bspc rule -a Screenkey manage=off
-
-    #echo | xmodmap - <<END
-    #  remove Mod4 = Super_L
-    #  remove Control = Control_L
-    #  keysym Control_L = Super_L
-    #  keysym Super_L = Control_L
-    #  add Mod4 = Super_L
-    #  add Control = Control_L
-    #END
   '';
   home.file.".Xmodmap".text = ''
     remove Mod4 = Super_L
@@ -215,6 +212,19 @@
           inherit (flakes.self) narHash lastModified;
           path = flakes.self.outPath;
           rev = if flakes.self ? rev then flakes.self.rev
+                else "0000000000000000000000000000000000000000";
+          type = "path";
+        };
+      }
+      {
+        from = {
+          id = "nixpkgs";
+          type = "indirect";
+        };
+        to = {
+          inherit (flakes.stable) narHash lastModified;
+          path = flakes.stable.outPath;
+          rev = if flakes.stable ? rev then flakes.stable.rev
                 else "0000000000000000000000000000000000000000";
           type = "path";
         };
