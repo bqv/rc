5369e06eecb1ed09d77a061020b9cf0446cd4aa3
Tony Olagbaiye <me@fron.io>
2020-11-06 12:44:48 +0000

    overlays::xf86videointel: fix build

---
+++ overlays/xf86videointel.nix
@@ -0,0 +1,7 @@
+final: prev: {
+  xorg = prev.xorg // prev.xorg.overrideScope' (self: super: rec {
+    xf86videointel = super.xf86videointel.overrideAttrs (drv: {
+      buildInputs = drv.buildInputs ++ [ self.libXv ];
+    });
+  });
+}
