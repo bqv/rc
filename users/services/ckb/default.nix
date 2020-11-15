b26d348c03f7992477053b4c1241067c91eb7a99
Tony Olagbaiye <me@fron.io>
2020-11-15 12:04:39 +0000

    users::ckb: set ckb-next config declaratively

--- users/services/ckb/default.nix
+++ users/services/ckb/default.nix
@@ -6,6 +6,9 @@ in {
   options.services.ckb.enable = lib.mkEnableOption "the ckb-next application";
 
   config = mkIf cfg.enable rec {
+    xdg.configFile."ckb-next/ckb-next.conf" = {
+      text = import ./conf.nix { inherit lib; };
+    };
     systemd.user.services.ckb-next = {
       Service = {
         ExecStart = "${pkgs.ckb-next}/bin/ckb-next --platform vnc:port=5995";
