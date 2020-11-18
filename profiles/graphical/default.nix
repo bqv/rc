bcef9e9e56548fb1864739dea13ddf5086315911
Tony Olagbaiye <me@fron.io>
2020-11-18 07:55:32 +0000

    profiles::graphical: update x setup

--- profiles/graphical/default.nix
+++ profiles/graphical/default.nix
@@ -11,7 +11,7 @@
   ];
 
   environment.systemPackages = with pkgs; [
-    velox
+    xorg.xinit velox
   ];
 
   hardware.opengl.enable = true;
@@ -25,11 +25,20 @@
       sddm = {
         enable = true;
         theme = "chili";
-        autoLogin.relogin = true;
         extraConfig = ''
           [X11]
           UserAuthFile=.local/share/sddm/Xauthority
         '';
+        setupCommands = ''
+          export XDG_RUNTIME_DIR=/run/user/$(id --user)
+          export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id --user)/bus
+
+          xrandr \
+            --output DisplayPort-1 --mode 1920x1080 --pos 0x270 --auto \
+            --output HDMI-A-1 --mode 1920x1080 --pos 1920x0 --scale 1.0x1.0 \
+            --output HDMI-A-0 --mode 1920x1080 --pos 4320x270 --auto \
+            --output DVI-D-0 --mode 1920x1080 --pos 6240x270 --auto
+        ''; # Scale was at 1.25 previously
       };
       autoLogin = {
         enable = true;
@@ -39,5 +48,6 @@
       defaultSession = "none+openbox";
     };
     windowManager.openbox.enable = true;
+    windowManager.awesome.enable = true;
   };
 }
--- users/services/mpd/default.nix
+++ users/services/mpd/default.nix
@@ -4,6 +4,10 @@ with lib; let
   cfg = config.services.mpd;
 in {
   config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      mpc_cli mpg123
+    ];
+
     services.mpd = {
       musicDirectory = config.xdg.userDirs.music;
       network.listenAddress = "any";
