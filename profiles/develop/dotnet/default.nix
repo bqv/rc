32f6080f648fafd8bd1e9b5aeca91ea201663730
Tony Olagbaiye <me@fron.io>
2020-11-30 12:34:01 +0000

    profiles::dotnet: fix collisions

--- profiles/develop/dotnet/default.nix
+++ profiles/develop/dotnet/default.nix
@@ -5,7 +5,7 @@
     (with dotnetCorePackages; combinePackages [
       sdk_2_1 sdk_3_0 sdk_3_1
     ])
-    mono msbuild
+    mono (lowPrio msbuild)
     azure-cli
     dotnetPackages.azure-functions-core-tools
    #jetbrains.rider
