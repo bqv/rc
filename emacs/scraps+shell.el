2115d5c6e495ee49f25535352c71b9e010c69aa4
Tony Olagbaiye <me@fron.io>
2020-12-05 17:46:15 +0000

    autosave emacs/scraps+shell.el after stage

--- emacs/scraps+shell.el
+++ emacs/scraps+shell.el
@@ -1,3 +1,9 @@
 (setq explicit-shell-file-name (executable-find "bash"))
 (setq shell-file-name (executable-find "bash"))
 (do-buffers (setenv "PAGER" "cat"))
+
+(defun shell-region (start end)
+  "execute region in an inferior shell"
+  (interactive "r")
+  (shell-command (buffer-substring-no-properties start end)))
+(global-set-key (kbd "C-x !") #'shell-region)
