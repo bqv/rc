c9c79cce42eec94403eda087fba5b7606f025984
Tony Olagbaiye <me@fron.io>
2020-12-06 21:24:40 +0000

    nyxt: misc updates

--- overlays/nyxt.nix
+++ overlays/nyxt.nix
@@ -22,23 +22,27 @@ in {
                                   (drv.propagatedBuildInputs or []);
     });
 
+    trivialBuild = args@{ baseName, ... }: lispPackages.buildLispPackage ({
+      inherit baseName;
+      description = baseName;
+      packageName = baseName;
+      buildSystems = [ baseName ];
+      asdFilesToKeep = [ "${baseName}.asd" ];
+    } // args);
+
     iterate' = quicklispPackages.iterate.overrideAttrs (drv: {
       nativeBuildInputs = [ quicklispPackages.rt ];
     });
-    cl-syslog' = lispPackages.buildLispPackage {
+    cl-syslog' = trivialBuild rec {
       baseName = "cl-syslog";
       version = "2006-11-28";
-      description = "cl-syslog";
       deps = [ quicklispPackages.cffi ];
-      buildSystems = [ "cl-syslog" ];
       src = final.fetchFromGitHub {
         owner = "lhope";
         repo = "cl-syslog";
         rev = "f62d524";
         sha256 = "rH0zOgXR3BixtVaqcYPhIfZBjqYgWnqrORQfr1mBqFM=";
       };
-      packageName = "cl-syslog";
-      asdFilesToKeep = [ "cl-syslog.asd" ];
     };
     log4cl' = quicklispPackages.log4cl.overrideAttrs (drv: {
       nativeBuildInputs = [ cl-syslog' ];
@@ -53,28 +57,22 @@ in {
       installPhase = lib.replaceStrings ["nyxt-ext"] ["nyxt"] drv.installPhase;
     });
 
-    eager-future2 = lispPackages.buildLispPackage {
+    eager-future2 = trivialBuild rec {
       baseName = "eager-future2";
       version = "2011-02-07";
-      description = "eager-future2";
       deps = [
         quicklispPackages.bordeaux-threads
         quicklispPackages.trivial-garbage
       ];
-      buildSystems = [ "eager-future2" ];
       src = final.fetchzip {
         url = "https://common-lisp.net/project/eager-future/release/eager-future2-0.2.tgz";
         sha256 = "lkLFUkTgWoOd9AMLJkqpYqP1pgpSyO4xJ4ueFm3PuFE=";
       };
-      packageName = "eager-future2";
-      asdFilesToKeep = [ "eager-future2.asd" ];
     };
-    jpl-util = lispPackages.buildLispPackage {
+    jpl-util = trivialBuild rec {
       baseName = "jpl-util";
       version = "2011-06-14";
-      description = "jpl-util";
       deps = [];
-      buildSystems = [ "jpl-util" ];
       src = final.fetchFromGitHub {
         owner = "hawkir";
         repo = "cl-jpl-util";
@@ -82,36 +80,28 @@ in {
         sha256 = "0nc0rk9n8grkg3045xsw34whmcmddn2sfrxki4268g7kpgz0d2yz";
         # date = 2015-10-05T11:08:40-07:00;
       };
-      packageName = "jpl-util";
-      asdFilesToKeep = [ "jpl-util.asd" ];
     };
-    jpl-queues = lispPackages.buildLispPackage {
+    jpl-queues = trivialBuild rec {
       baseName = "jpl-queues";
       version = "2009-10-19";
-      description = "jpl-queues";
       deps = [
         quicklispPackages.bordeaux-threads
         jpl-util
       ];
-      buildSystems = [ "jpl-queues" ];
       src = final.fetchzip {
         url = "http://www.thoughtcrime.us/software/jpl-queues/jpl-queues-0.1.tar.gz";
         sha256 = "CMl5j4uzTlP6SSSm6Z3gKHZdhyRgMNqqZCC/oJxu7fU=";
       };
-      packageName = "jpl-queues";
-      asdFilesToKeep = [ "jpl-queues.asd" ];
     };
-    calispel = lispPackages.buildLispPackage {
+    calispel = trivialBuild rec {
       baseName = "calispel";
       version = "2009-10-19";
-      description = "calispel";
       deps = [
         quicklispPackages.bordeaux-threads
         eager-future2
         jpl-queues
         jpl-util
       ];
-      buildSystems = [ "calispel" ];
       src = final.fetchFromGitHub {
         owner = "hawkir";
         repo = "calispel";
@@ -119,25 +109,43 @@ in {
         sha256 = "08bmf3pi7n5hadpmqqkg65cxcj6kbvm997wcs1f53ml1nb79d9z8";
         # date = 2014-10-27T17:47:36+00:00;
       };
-      packageName = "calispel";
-      asdFilesToKeep = [ "calispel.asd" ];
+    };
+
+    slynk = trivialBuild rec {
+      baseName = "slynk";
+      deps = [
+        quicklispPackages.bordeaux-threads
+      ];
+      inherit (final.emacsPackages.sly) src version;
+      overrides = p: {
+        postUnpack = "src=$src/slynk";
+      };
     };
 
     nyxtPkgs.nyxt = (lib.foldl (lib.flip deepOverride) nyxt' [
       log4cl' iterate'
-    ]).overrideAttrs (drv: {
+    ]).overrideAttrs (drv: rec {
+      qlSetup = "${lispPackages.quicklisp}/lib/common-lisp/quicklisp/quicklisp/setup.lisp";
       propagatedBuildInputs = drv.propagatedBuildInputs ++ [
+        lispPackages.quicklisp
         calispel
+        slynk
+       #slynk-quicklisp
+       #slynk-asdf
+       #slynk-named-readtables
+       #slynk-macrostep
       ];
       postInstall = lib.replaceStrings [
         "sb-alien::*shared-objects*"
         "\"\""
         "='"
       ] [
-        "(setf cffi:*foreign-library-directories*
-           (cffi::explode-path-environment-variable
-             \\\"NIX_LISP_LD_LIBRARY_PATH\\\"))"
-        "\"cffi\""
+        "(progn
+           ;(cl:load \"${qlSetup}\" :verbose t :print t)
+           (setf cffi:*foreign-library-directories*
+             (cffi::explode-path-environment-variable
+               \\\"NIX_LISP_LD_LIBRARY_PATH\\\")))"
+        "\"cffi slynk\""
         "='\nset +x"
       ] drv.postInstall;
     });
