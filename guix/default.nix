da0fd50678a3a0d91136008a2489cb4278115618
Tony Olagbaiye <me@fron.io>
2020-12-24 01:15:10 +0000

    guix(channels): add nongnu and gaming

--- guix/default.nix
+++ guix/default.nix
@@ -8,6 +8,18 @@ let
       introduction = "b5f32950a8fa9c05efb27a0014f9b336bb318d69";
       fingerprint = "1EFB 0909 1F17 D28C CBF9  B13A 53D4 57B2 D636 EE82";
     };
+    nonguix = {
+      name = "nonguix";
+      url = "https://gitlab.com/nonguix/nonguix";
+      introduction = "897c1a470da759236cc11798f4e0a5f7d4d59fbc";
+      fingerprint = "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5";
+    };
+    gaming = {
+      name = "guix-gaming-games";
+      url = "https://gitlab.com/guix-gaming-channels/games.git";
+      introduction = "c23d64f1b8cc086659f8781b27ab6c7314c5cca5";
+      fingerprint = "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F";
+    };
   };
 in {
   config = rec {
