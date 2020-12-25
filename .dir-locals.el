a2ced5bbf46684e9eebceab32cb969fe7fcab5de
Tony Olagbaiye <me@fron.io>
2020-12-25 19:22:18 +0000

    users(nyxt): update search engine

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,7 +4,7 @@
 ((nil . (
         ;(eval . (set (make-local-variable 'default-directory)
         ;             (locate-dominating-file buffer-file-name ".dir-locals.el")))
-         (compile-command . "systemd-run --user --scope env TERM=dumb nix run '.#delta'")
+         (compile-command . "env TERM=dumb nix run '.#delta'")
          (eval . (setq projectile-project-compilation-cmd
-                       "systemd-run --user -u nixos --scope if env TERM=dumb nix build '' if ./result '' git push"))
+                       "env TERM=dumb if nix build '' if ./result '' git push"))
          )))
