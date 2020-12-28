e82c322729e3ee85b26f9d17bf8b1ba3dbfbe5b8
Tony Olagbaiye <me@fron.io>
2020-12-28 01:15:45 +0000

    emacs: add sly

--- emacs/sly.nix
+++ emacs/sly.nix
@@ -4,26 +4,31 @@
   emacs-loader.sly = {
     demand = true;
     config = ''
+      nil
     '';
   };
   emacs-loader.sly-quicklisp = {
     demand = true;
     config = ''
+      nil
     '';
   };
   emacs-loader.sly-asdf = {
     demand = true;
     config = ''
+      nil
     '';
   };
   emacs-loader.sly-named-readtables = {
     demand = true;
     config = ''
+      nil
     '';
   };
   emacs-loader.sly-macrostep = {
     demand = true;
     config = ''
+      nil
     '';
   };
 }
