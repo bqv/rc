d0c9f85e64fbc9846f84e7cf59d34fca628def2d
Tony Olagbaiye <me@fron.io>
2020-12-29 15:17:21 +0000

    autosave modules/misc/attrmods.nix after stage

--- modules/misc/attrmods.nix
+++ modules/misc/attrmods.nix
@@ -1,31 +1,42 @@
-{ config, options, lib, ... }:
+args@{ config, options, lib, ... }:
 
 let
-  nixos = options ? environment;
-  home-manager = options ? home;
+  # Slightly flimsy but means I don't need two files for this.
+  nixos = ! home-manager;
+  home-manager = args ? nixosConfig;
 in {
   options = builtins.foldl' (a: b: a // b) {} [
     (if home-manager then {
+
       home.packages' = lib.mkOption {
         type = lib.types.attrs;
+        description = "Attrset based mirror of home.packages";
       };
+
     } else {})
     (if nixos then {
+
       environment.systemPackages' = lib.mkOption {
         type = lib.types.attrs;
+        description = "Attrset based mirror of environment.systemPackages";
       };
+
     } else {})
   ];
   config = builtins.foldl' (a: b: a // b) {} [
     (if home-manager then {
+
       home.packages' = lib.mkOverride 100 (lib.zipAttrs
         (map (p: { ${if p ? pname then p.pname else p.name} = p; })
           config.home.packages));
+
     } else {})
     (if nixos then {
+
       environment.systemPackages' = lib.mkOverride 100 (lib.zipAttrs
         (map (p: { ${if p ? pname then p.pname else p.name} = p; })
           config.environment.systemPackages));
+
     } else {})
   ];
 }
