5b186bcbf2489f8abd6734432869fc35189a2a23
Tony Olagbaiye <me@fron.io>
2021-01-05 10:06:57 +0000

    profiles(pipewire): fix emulation

---
+++ profiles/services/hydra/default.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, lib, domains, ... }:
+
+let
+  cfg = config.services.hydra;
+in {
+  services.hydra = lib.mkIf cfg.enable {
+    hydraURL = "http://localhost:3000";
+    notificationSender = "hydra@${domains.home}";
+    buildMAchinesFiles = [];
+    useSubstitutes = true;
+  };
+}
--- profiles/sound/pipewire.nix
+++ profiles/sound/pipewire.nix
@@ -2,9 +2,13 @@
 
 {
   services.pulseaudio = lib.mkForce false;
+  hardware.pulseaudio.enable = lib.mkForce false;
 
   services.pipewire = {
     enable = true;
+    alsa.enable = true;
+    jack.enable = true;
+    pulse.enable = true;
   };
 
   xdg.portal = {
