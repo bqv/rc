a4a857c03852b7147d31ccf1e28a8a2c66488a6f
Tony Olagbaiye <me@fron.io>
2021-01-06 20:57:08 +0000

    emacs(shell): more shell shortcuts

--- emacs/scraps+shell.el
+++ emacs/scraps+shell.el
@@ -44,10 +44,12 @@
   (interactive "p")
   (ivy-shell nil :initial-input (if (= pfx -1) (car shell-command-history))))
 (global-set-key (kbd "M-!") #'ivy-shell-sync)
+(global-set-key (kbd "´") #'ivy-shell-sync)
 (defun ivy-shell-async (&optional pfx)
   (interactive "p")
   (ivy-shell t :initial-input (if (= pfx -1) (car shell-command-history))))
 (global-set-key (kbd "M-&") #'ivy-shell-async)
+(global-set-key (kbd "`") #'ivy-shell-async)
 
 (provide 'scraps+shell)
 ;;; scraps+shell.el ends here
