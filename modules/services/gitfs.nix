02b66a7d5aa4f6a6c7e9167ba9973a5b3762d177
Tony Olagbaiye <me@fron.io>
2021-01-07 07:26:11 +0000

    overlays(gitfs): fix gitfs

---
+++ modules/services/gitfs.nix
@@ -0,0 +1,161 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.services.gitfs;
+in {
+  options.services.gitfs = with lib; {
+    enable = mkEnableOption "gitfs";
+
+    workdir = mkOption {
+      type = types.str;
+      default = "/var/lib/gitfs";
+      description = ''
+        The internal gitfs working directory.
+      '';
+    };
+
+    mounts = mkOption {
+      type = types.submodule {
+        options = { name, config, ... }: {
+          directory = mkOption {
+            type = types.path;
+            default = name;
+            description = ''
+              The directory where nixos.git will be mounted.
+            '';
+          };
+
+          github = {
+            owner = mkOption {
+              type = with types; nullOr str;
+              default = null;
+              example = "bqv";
+              description = ''
+                Owner of the repository hosted on GitHub.
+              '';
+            };
+
+            repo = mkOption {
+              type = with types; nullOr str;
+              default = null;
+              example = "nixrc";
+              description = ''
+                Name of the repository hosted on GitHub.
+              '';
+            };
+
+            githubRemote = mkOption {
+              type = types.str;
+              internal = true;
+              default = with config.github; "http://github.com/${owner}/${repo}";
+            };
+          };
+
+          remote = mkOption {
+            type = with types; nullOr str;
+            default = null;
+            example = "http://github.com/bqv/nixrc";
+            description = ''
+              Url of the repository.
+            '';
+          };
+
+          branch = mkOption {
+            type = types.str;
+            default = "master";
+            example = "live";
+            description = ''
+              Name of the branch of the repository to be mounted.
+            '';
+          };
+
+          extraParams = mkOption {
+            type = types.attrs;
+            default = {};
+            example = { idle_fetch_timeout = "10"; };
+            description = ''
+              Extra params to pass to the mounter.
+            '';
+          };
+
+          invocation = mkOption {
+            type = types.submodule {
+              options = { name, config, ... }: {
+                url = mkOption { type = types.str; };
+                params = mkOption { type = types.str; };
+              };
+            };
+            internal = true;
+            default = {
+              url = if ((mnt.github.owner == null) || (mnt.github.repo == null))
+                    then mnt.remote else mnt.githubRemote;
+              params = let
+                paramAttrs = mnt.extraParams // {
+                  branch = mnt.branch;
+                  foreground = "true";
+                  allow_other = "true";
+                };
+                toKVList = lib.mapAttrsToList (k: v: "${k}=${builtins.toString v}");
+              in concatStringsSep "," (toKVList paramAttrs);
+            };
+          };
+        };
+      };
+      default = {
+      };
+    };
+
+    package = mkOption {
+      type = types.package;
+      default = pkgs.gitfs;
+      defaultText = "pkgs.gitfs";
+      description = ''
+        Which gitfs package to use.
+      '';
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = builtins.all (mnt: !(builtins.all (v: v == null)
+          [ mnt.github.owner mnt.github.repo mnt.remote ])) cfg.mounts;
+        message = ''
+          You must set either remote or github in services.gitfs.mounts.
+        '';
+      }
+      {
+        assertion = builtins.all (mnt: ((mnt.github.owner == null) == (mnt.github.repo == null))) cfg.mounts;
+        message = ''
+          You must set both owner and repo in services.gitfs.github.mounts.
+        '';
+      }
+    ];
+
+    environment.systemPackages = [ cfg.package ];
+
+    programs.fuse.userAllowOther = true;
+
+    systemd.services = lib.mapAttrs' (name: mnt: {
+      name = lib.strings.sanitizeDerivationName "gitfs-${name}";
+      value = {
+        enable = true;
+        path = [ cfg.package pkgs.coreutils ];
+        after = [ "network.target" ];
+        description = "${mnt.directory} gitfs mount of ${mnt.invocation.url}";
+        environment.HOME = cfg.workdir;
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          ExecStartPre = ''
+            ${pkgs.coreutils}/bin/mkdir -p ${cfg.workdir} ${mnt.directory}
+          '';
+          ExecStart = ''
+            ${cfg.package}/bin/gitfs ${mnt.invocation.url} -o ${mnt.invocation.params} ${mnt.directory}
+          '';
+          Restart = "always";
+          RestartSec = 15;
+        };
+      };
+    }) cfg.mounts;
+  };
+}
