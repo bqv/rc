ca53c692c4e1cba608432033f95ed17dee582484
Tony Olagbaiye <me@fron.io>
2021-01-07 05:09:53 +0000

    emacs(misc): slower volume control

--- emacs/scraps+misc.el
+++ emacs/scraps+misc.el
@@ -40,6 +40,11 @@ See `comint-run'."
     (bookmark-save)))
 (advice-add 'bookmark-set :after #'bookmark-save-advice)
 
+(setq desktop-environment-volume-get-command "amixer -c0 get Master")
+(setq desktop-environment-volume-set-command "amixer -c0 set Master %s")
+(setq desktop-environment-volume-toggle-command "amixer -c0 set Master toggle")
 (define-key desktop-environment-mode-map (kbd "<269025043>") #'desktop-environment-volume-increment) ; mouse v-up
+(define-key desktop-environment-mode-map (kbd "S-<269025043>") #'desktop-environment-volume-increment-slowly)
 (define-key desktop-environment-mode-map (kbd "<269025041>") #'desktop-environment-volume-decrement) ; mouse v-down
+(define-key desktop-environment-mode-map (kbd "S-<269025041>") #'desktop-environment-volume-decrement-slowly)
 (define-key desktop-environment-mode-map (kbd "<269025073>") #'emms-pause) ; headset btn
