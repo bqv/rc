658ca791bebe71bdf7dc8443f56f8cda13ca5cfa
Tony Olagbaiye <me@fron.io>
2021-01-07 02:57:46 +0000

    hosts(delta): enable bcachefs

--- profiles/bcachefs.nix
+++ profiles/bcachefs.nix
@@ -1,13 +1,24 @@
 { config, pkgs, lib, ... }:
 
 {
-  boot.kernelPackages = pkgs.large.linuxPackages_testing_bcachefs;
+  disabledModules = [ "tasks/filesystems/zfs.nix" ];
+
+  environment.systemPackages = [ pkgs.bcachefs-tools ];
+
   boot.supportedFilesystems = [ "bcachefs" ];
+
+  boot.kernelPackages = lib.mkForce pkgs.large.linuxPackages_testing_bcachefs;
   boot.kernelPatches = [{
     name = "bcachefs-acl";
     patch = null;
     extraConfig = ''
       BCACHEFS_POSIX_ACL y
+    '';
+  } {
+    name = "bcachefs-debug";
+    patch = null;
+    extraConfig = ''
+      BCACHEFS_DEBUG y
     '';
   }];
 }
