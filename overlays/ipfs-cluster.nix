f4b6484ad22a7d4f67beaff6d02cdf9ce3f4fb63
Tony Olagbaiye <me@fron.io>
2021-01-08 03:20:59 +0000

    autosave overlays/ipfs-cluster.nix after stage

---
+++ overlays/ipfs-cluster.nix
@@ -0,0 +1,12 @@
+inputs@{...}: final: prev: {
+  ipfs-cluster = if lib.versionAtLeast prev.ipfs-cluster.version "0.13.1" then
+    (with prev.ipfs-cluster; final.buildGoModule {
+      inherit pname version doCheck meta;
+      src = final.fetchgit {
+        url = src.meta.homepage;
+        sha256 = "c9amvmxU+3P14NygJK2hTNR+lTdFK7eEj1eoqv2FTDs=";
+      };
+      vendorSha256 = "bejsbVMp5HnfUgG6pETK8Beeiax+BItVQh57sgmFbik=";
+    })
+  else builtins.trace "pkgs.ipfs-clpster: overlay expired" prev.ipfs-cluster;
+}
