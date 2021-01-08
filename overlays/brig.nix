4bb5ecc3c8b15078529e09d5eac8bc9b720c2b1d
Tony Olagbaiye <me@fron.io>
2021-01-08 06:39:53 +0000

    autosave overlays/brig.nix after stage

--- overlays/brig.nix
+++ overlays/brig.nix
@@ -2,11 +2,10 @@ inputs@{ brig, ... }: final: prev: {
   brig = if final.lib.versionAtLeast prev.brig.version "0.5.0"
   then builtins.trace "pkgs.brig: overlay expired" prev.brig
   else (with prev.brig; final.buildGoModule {
-    inherit pname version doCheck meta;
-    src = final.fetchgit {
-      url = src.meta.homepage;
-      sha256 = "c9amv3xU+3P14NygJK2hTNR+lTdFK7eEj1eoqv2FTDs=";
-    };
-    vendorSha256 = "bajsbVMp5HnfUgG6pETK8Beeiax+BItVQh57sgmFbik=";
+    name = "brig-${inputs.brig.lastModifiedDate}";
+    inherit pname doCheck meta;
+    version = inputs.brig.shortRev;
+    src = inputs.brig;
+    vendorSha256 = "9kVTRRQWJq3gMsIqrsJobOZTucF2beDiHwXOA1LaAOQ=";
   });
 }
