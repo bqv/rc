2d6fd8fc92b78324a5c6ad14304146812143ef98
Tony Olagbaiye <me@fron.io>
2021-01-09 03:06:08 +0000

    autosave .dir-locals.el after stage

--- .dir-locals.el
+++ .dir-locals.el
@@ -6,5 +6,5 @@
         ;             (locate-dominating-file buffer-file-name ".dir-locals.el")))
          (compile-command . "env TERM=dumb nix run -vv '.#delta' --show-trace")
          (eval . (setq projectile-project-compilation-cmd
-                       "env TERM=dumb if nix build -vv --show-trace '' if ./result '' git push"))
+                       "env TERM=dumb nix build -vv --show-trace && ./result && git push"))
          )))
