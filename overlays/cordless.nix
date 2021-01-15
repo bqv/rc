465b02c6ac5266803e0d378fbc54863f866010de
Tony Olagbaiye <me@fron.io>
2021-01-15 01:06:40 +0000

    autosave overlays/cordless.nix after stage

--- overlays/cordless.nix
+++ overlays/cordless.nix
@@ -1,13 +1,12 @@
 inputs@{...}: final: prev: {
-  cordless = with prev.cordless; final.buildGoModule {
-    name = "cordless";
-    src = pkgs.fetchFromGitHub {
+  cordless = prev.buildGoModule {
+    src = prev.fetchFromGitHub {
       owner = "bios-marcel";
       repo = "cordless";
       rev = "2d5a3c39eb9d2fdd934c3cb5c3c7ebfc2f5c6761";
       sha256 = "1k78jna4aqvai94hg021khl9d4lmkgxgdqrr32k8vwfm48310f8i";
     };
     vendorSha256 = "XnwTqd19q+hOJZsfnFExiPDbg4pzV1Z9A6cq/jhcVgU=";
-    inherit (pkgs.cordless) version meta;
+    inherit (prev.cordless) pname version meta;
   };
 }
