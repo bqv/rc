ded11f681b383f4a2010a1703982fff7e26c7edf
Tony Olagbaiye <me@fron.io>
2021-01-16 08:58:47 +0000

    containers(jellyfin): use tmpfiles to set data permissions

--- overlays/pipewire.nix
+++ overlays/pipewire.nix
@@ -1,12 +1,12 @@
 inputs@{...}: final: prev: {
   pipewire = if final.lib.versionAtLeast prev.pipewire.version "0.3.20"
   then builtins.trace "pkgs.pipewire: overlay expired" prev.pipewire
-  else (with prev.pipewire; overrideAttrs {
+  else prev.pipewire.overrideAttrs {
     version = "0.3.19-git";
     src = final.fetchgit {
-      url = src.meta.homepage;
+      url = prev.pipewire.src.meta.homepage;
       rev = "5f66650f7c99545369895539aa0824d285d39370";
       sha256 = "c9amvmxU03P14NygJK2hTNR0lTdFK7eEj1eoqv2FTDs=";
     };
-  });
+  };
 }
