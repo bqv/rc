1858e00b7108a3624f62c767fc52a8da0cd55759
Tony Olagbaiye <me@fron.io>
2021-01-16 07:10:10 +0000

    emacs(undo-tree): set, for evil

--- .dir-locals.el
+++ .dir-locals.el
@@ -6,5 +6,5 @@
         ;             (locate-dominating-file buffer-file-name ".dir-locals.el")))
          (compile-command . "env TERM=dumb nix run -vv '.#delta' --show-trace")
          (eval . (setq projectile-project-compilation-cmd
-                       "env TERM=dumb nix build -vv --show-trace && ./result && git push"))
+                       "env TERM=dumb nix build --show-trace && ./result && git push"))
          )))
--- emacs/undo-tree.nix
+++ emacs/undo-tree.nix
@@ -3,8 +3,11 @@
 {
   emacs-loader.undo-tree = {
     demand = true;
+    after = [ "evil" ];
     config = ''
       (global-undo-tree-mode t)
+      (evil-set-undo-system 'undo-tree)
+      (setq undo-tree-enable-undo-in-region nil)
     '';
   };
 }
