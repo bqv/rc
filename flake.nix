48ef930e1663417f35a11bdfc915a1c2d001e146
Tony Olagbaiye <me@fron.io>
2021-01-21 13:10:11 +0000

    flake(lock): add pmgmt

--- flake.nix
+++ flake.nix
@@ -77,6 +77,7 @@
       pipeliner = { url = "github:anki-code/xontrib-pipeliner/daccb6c8a67bbda799dfa2d6d8d829b5e9151c92"; flake = false; };
     };
 
+    processmgmt = { url = "github:svanderburg/nix-processmgmt/s6-rc"; flake = false; }; # ProcessMgmt
     hnix-overlay = { url = "github:haskell-nix/hnix"; flake = false; };            # Hnix
     impermanence = { url = "github:nix-community/impermanence"; flake = false; };  # Impermanence
     mozilla = { url = "github:mozilla/nixpkgs-mozilla"; flake = false; };          # Nixpkgs-mozilla
