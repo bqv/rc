6a97a1f02e75b2ac57b1fa4cbdd55939daff483d
Tony Olagbaiye <me@fron.io>
2021-01-27 00:16:35 +0000

    autosave modules/system/machines.nix after stage

--- modules/system/machines.nix
+++ modules/system/machines.nix
@@ -1,6 +1,8 @@
 { config, pkgs, lib, ... }:
 
-{
+let
+  cfg = config.isolation;
+in {
   options.isolation = with lib; {
     makeHostAddress = mkOption {
       type = types.function;
@@ -11,47 +13,79 @@
       exampleText = ''{ id, ... }: "10.''${id}".0.2'';
     };
     scopes = mkOption {
-      type = types.listOf types.submodule ({ config, ... }: {
+      type = types.addCheck (types.listOf types.submodule ({ config, ... }: {
         options = {
           id = mkOption {
-            type = types.addCheck types.int (x: x>=0 && x<256);
+            type = types.ints.u8;
           };
           name = mkOption {
             type = types.str;
           };
-          system = mkOption {
-            type = types.listOf types.submodule ({ config, ... }: {
-                          boot.isContainer = true;
-                          networking.hostName = mkDefault name;
-                          networking.useDHCP = false;
-                          assertions = [
-                            {
-                              assertion =  config.privateNetwork -> stringLength name < 12;
-                              message = ''
-                                Container name `${name}` is too long: When `privateNetwork` is enabled, container names can
-                                not be longer than 11 characters, because the container's interface name is derived from it.
-                                This might be fixed in the future. See https://github.com/NixOS/nixpkgs/issues/38509
-                              '';
-                            }
-                          ];
+          hostAddress = mkOption {
+            type = types.str;
+            default = cfg.makeHostAddress {
+              inherit (config) id name;
+            };
+          };
+          localAddress = mkOption {
+            type = types.str;
+            default = cfg.makeLocalAddress {
+              inherit (config) id name;
+            };
+          };
+          nixos = mkOption {
+            type = types.listOf types.submodule (let
+              inherit (config) name;
+            in { config, ... }: {
+              boot.isContainer = true;
+              networking.hostName = mkDefault name;
+              networking.useDHCP = false;
+              assertions = [
+                {
+                  assertion =  config.privateNetwork -> lib.stringLength name < 12;
+                  message = ''
+                    Container name `${name}` is too long: When `privateNetwork` is enabled, container names can
+                    not be longer than 11 characters, because the container's interface name is derived from it.
+                    This might be fixed in the future. See https://github.com/NixOS/nixpkgs/issues/38509
+                  '';
+                }
+              ];
             });
+          };
+          system = mkOption {
+            type = types.package;
+            default = config.nixos.config.system.build.toplevel;
+            internal = true;
           };
         };
         config = {
-          _module.args.hostAddress = cfg.makeHostAddress {
-            inherit (config) id name;
-          };
-          _module.args.localAddress = cfg.makeLocalAddress {
-            inherit (config) id name;
-          };
+          _module.args = { inherit (config) hostAddress localAddress; };
         };
-      });
+      })) (xs: let
+        left = builtins.attrNames (builtins.listToAttrs (map (x: {
+          inherit (x) name; value = null;
+        }) xs));
+        right = map (x: x.name) xs;
+      in lib.length left == lib.length right);
+    };
+    machines = mkOption {
+      type = types.attrsOf types.package;
+      default = builtins.listToAttrs (map (value: {
+        inherit (value) name;
+        inherit value;
+      }) config.scopes);
+      internal = true;
     };
   };
 
   config = {
-    nix.extraOptions = ''
-      extra-platforms = ${toString config.nix.supportedPlatforms} i686-linux
-    '';
+    containers = lib.mapAttrs (machine: {
+      autoStart = true;
+      enableTun = true;
+      privateNetwork = true;
+      inherit (machine) hostAddress localAddress;
+
+      config.system.build.toplevel = machine.system;
+    }) cfg.machines;
   };
 }
