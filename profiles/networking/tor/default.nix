1e64a50df0ab76262131f8f88eb9ec968c751f7a
Tony Olagbaiye <me@fron.io>
2021-02-03 16:18:02 +0000

    autosave profiles/networking/tor/default.nix after save

--- profiles/networking/tor/default.nix
+++ profiles/networking/tor/default.nix
@@ -15,7 +15,6 @@
       ContactInfo = "tor+${domains.srvc}@${domains.home}";
       Nickname = config.networking.hostName;
       ORPort = 143;
-      DNSPort = [{ addr = "127.0.0.1"; port = 9053; }];
       ControlPort = [ 9051 ];
     };
   };
