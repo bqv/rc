4bb6d85b57e71208b050f59a9df52fbec2ebeb3b
Tony Olagbaiye <me@fron.io>
2021-02-04 13:03:11 +0000

    autosave pkgs/default.nix after stage

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -71,6 +71,26 @@ in rec {
 
   shflags = prev.callPackage ./tools/misc/shflags { };
 
+  taiwins = prev.stdenv.mkDerivation {
+    name = "taiwins";
+    src = prev.fetchgit {
+      url = "https://github.com/taiwins/taiwins";
+      rev = "722503b9eed47a8cc2dd53dd0b5b03d2f0a50c3f";
+      sha256 = "l1DKuH8Q2Mp3NAFqe7weHPeXwh1Ux9eJ+f4gLE0qmNA=";
+      fetchSubmodules = true;
+    };
+    nativeBuildInputs = with prev; [
+      meson pkgconfig libxkbcommon wayland cairo cmake udev librsvg
+      wayland-protocols lua5_3 dbus libdrm libinput mesa pam ninja pixman
+      xorg.xcbutil xorg.libxcb xorg.xcbutilwm xorg.xcbutilerrors
+    ];
+    CFLAGS = [
+      "-Wno-error=unused-result"
+      "-Wno-error=maybe-uninitialized"
+      "-Wno-error=stringop-overflow="
+    ];
+  };
+
   twitterpub = prev.callPackage ./servers/twitterpub { };
 
   velox = prev.callPackage ./applications/window-managers/velox { };
