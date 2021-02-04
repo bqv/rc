7917f0c276678181c0ed3694309be7f3dfbb1c78
Tony Olagbaiye <me@fron.io>
2021-02-04 12:48:36 +0000

    autosave pkgs/default.nix after save

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -73,9 +73,22 @@ in rec {
 
   taiwins = prev.stdenv.mkDerivation {
     name = "taiwins";
-    src = prev.fetchgit { url = "https://github.com/taiwins/taiwins"; rev = "722503b9eed47a8cc2dd53dd0b5b03d2f0a50c3f"; fetchSubmodules = true; sha256 = "l1DKuH8Q2Mp3NAFqe7weHPeXwh1Ux9eJ+f4gLE0qmNA="; };
-    nativeBuildInputs = with prev; [ meson pkgconfig libxkbcommon wayland cairo cmake udev librsvg wayland-protocols lua5_3 dbus libdrm libinput mesa pam ninja pixman xorg.xcbutil xorg.libxcb xorg.xcbutilwm xorg.xcbutilerrors ];
-    CFLAGS = [ "-Wno-error=unused-result" "-Wno-error=maybe-uninitialized" "-Wno-error=stringop-overflow=" ];
+    src = prev.fetchgit {
+      url = "https://github.com/taiwins/taiwins";
+      rev = "722503b9eed47a8cc2dd53dd0b5b03d2f0a50c3f";
+      fetchSubmodules = true;
+      sha256 = "l1DKuH8Q2Mp3NAFqe7weHPeXwh1Ux9eJ+f4gLE0qmNA=";
+    };
+    nativeBuildInputs = with prev; [
+      meson pkgconfig libxkbcommon wayland cairo cmake udev librsvg
+      wayland-protocols lua5_3 dbus libdrm libinput mesa pam ninja pixman
+      xorg.xcbutil xorg.libxcb xorg.xcbutilwm xorg.xcbutilerrors
+    ];
+    CFLAGS = [
+      "-Wno-error=unused-result"
+      "-Wno-error=maybe-uninitialized"
+      "-Wno-error=stringop-overflow="
+    ];
   };
 
   twitterpub = prev.callPackage ./servers/twitterpub { };
