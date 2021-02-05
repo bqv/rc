91e4ade93f6172237795367a61eacaebbd3eda42
Tony Olagbaiye <me@fron.io>
2021-02-05 03:40:18 +0000

    autosave hosts/delta/default.nix after save

--- hosts/delta/default.nix
+++ hosts/delta/default.nix
@@ -292,7 +292,7 @@
     };
     init = pkgs.writeShellScript "s6-init" ''
       cd ${svdir}
-      ${pkgs.s6-rc}/bin/s6-rc-init && ${pkgs.s6-rc}/bin/s6-rc change default
+      ${pkgs.s6-rc}/bin/s6-rc-init . && ${pkgs.s6-rc}/bin/s6-rc change default
     '';
   in {
     inherit init;
