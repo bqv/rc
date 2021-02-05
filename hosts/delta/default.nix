d708604108ded1299fa8a27571aa3c4b36859703
Tony Olagbaiye <me@fron.io>
2021-02-05 14:16:57 +0000

    autosave hosts/delta/default.nix after save

--- hosts/delta/default.nix
+++ hosts/delta/default.nix
@@ -297,7 +297,7 @@
       ${pkgs.s6-rc}/bin/s6-rc-compile -v 3 $out ${svdir}/etc/s6/sv
     '';
     init = pkgs.writeShellScript "s6-init" ''
-      SCANDIR=/run/s6
+      SCANDIR=/var/run/s6-rc
       export PATH=${with pkgs; lib.makeBinPath [
         coreutils shadow tools.s6-rc tools.common
         s6 s6-rc s6-linux-utils s6-portable-utils execline
