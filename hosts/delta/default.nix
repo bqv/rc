19f41971a87b7d8a6bb01f11a16287e4f25df4c0
Tony Olagbaiye <me@fron.io>
2021-02-05 14:42:16 +0000

    autosave hosts/delta/default.nix after save

--- hosts/delta/default.nix
+++ hosts/delta/default.nix
@@ -315,7 +315,7 @@
       s6-svscan $SCANDIR & # cheaper than s6-linux-init
       PID=$!
       nixproc-s6-rc-deploy ${svdir}
-      ls /var/run
+      ls -la /var/run
       wait $PID
     '';
     test = pkgs.writeShellScript "go" ''
