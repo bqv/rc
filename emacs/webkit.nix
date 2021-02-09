f5fd711ee2f0fd45516f494b9d71e92bb1184491
Tony Olagbaiye <me@fron.io>
2021-02-09 21:49:50 +0000

    autosave emacs/webkit.nix after save

--- emacs/webkit.nix
+++ emacs/webkit.nix
@@ -21,7 +21,7 @@
     bind = {
       "M-*" = "webkit";
     };
-    init = lib.concatMapStringsSep "\n" ({ name, value }: ''
+    init = lib.concatMapStringsSep "" ({ name, value }: ''
       (setenv "${name}"
               (let ((cur (getenv "${name}"))
                     (new "${lib.concatStringsSep ":" value}"))
