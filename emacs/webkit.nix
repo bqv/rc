43468605543314cc7e9f178b96f05b56142674a6
Tony Olagbaiye <me@fron.io>
2021-02-09 22:34:02 +0000

    autosave emacs/webkit.nix after save

--- emacs/webkit.nix
+++ emacs/webkit.nix
@@ -38,6 +38,23 @@
       ;(setq browse-url-browser-function 'webkit-browse-url)
       (setq webkit-browse-url-force-new t)
       (setq webkit-dark-mode t)
+
+      ;; Override the "loading:" mode line indicator with an icon from `all-the-icons.el'
+      ;; You could also use a unicode icon like ↺
+      (defun webkit--display-progress (progress)
+        (setq webkit--progress-formatted
+              (if (equal progress 100.0)
+                  ""
+                (format "%s%.0f%%  " (all-the-icons-faicon "spinner") progress)))
+        (force-mode-line-update))
+
+      ;; Set action to be taken on a download request. Predefined actions are
+      ;; `webkit-download-default', `webkit-download-save', and `webkit-download-open'
+      ;; where the save function saves to the download directory, the open function
+      ;; opens in a temp buffer and the default function interactively prompts.
+      (setq webkit-download-action-alist '(("\\.pdf\\'" . webkit-download-open)
+                                           ("\\.png\\'" . webkit-download-save)
+                                           (".*" . webkit-download-default))
     '';
   };
 }
