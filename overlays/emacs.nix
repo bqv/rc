73e403ce82a56062fa8d02ff5d38c3ebddddc50e
Tony Olagbaiye <me@fron.io>
2021-02-09 16:57:04 +0000

    autosave overlays/emacs.nix after save

--- overlays/emacs.nix
+++ overlays/emacs.nix
@@ -69,39 +69,12 @@ in with prev.lib; rec {
     };
   });
 
-  emacsPgtkGccClient = final.stdenv.mkDerivation rec {
+  emacsPgtkGccClient = (final.stdenv.mkDerivation rec {
     pname = "emacsclient";
     inherit (src) version;
     src = final.emacsPgtkGcc;
     dontBuild = true;
-    nativeBuildInputs = [
-      final.wrapGAppsHook
-    ];
-    gstBuildInputs = with final; with gst_all_1; [
-      gstreamer gst-libav
-      gst-plugins-base
-      gst-plugins-good
-      gst-plugins-bad
-      gst-plugins-ugly
-    ];
-    buildInputs = with final; [
-      webkitgtk
-      glib gdk-pixbuf cairo
-      mime-types pango gtk3
-      glib-networking gsettings-desktop-schemas
-      xclip notify-osd enchant
-    ] ++ gstBuildInputs;
-
-    GIO_EXTRA_MODULES = "${final.glib-networking}/lib/gio/modules:${final.dconf.lib}/lib/gio/modules";
-    GST_PLUGIN_SYSTEM_PATH_1_0 = final.lib.concatMapStringsSep ":" (p: "${p}/lib/gstreamer-1.0") gstBuildInputs;
-    dontWrapGApps = true;
-    installPhase = ''
-      makeWrapper $src/bin/emacsclient $out/bin/emacsclient \
-        --prefix LD_LIBRARY_PATH : "${final.lib.makeLibraryPath buildInputs}" \
-        "''${gappsWrapperArgs[@]}" \
-        --argv0 emacsclient
-    '';
-  };
+  }).overrideAttrs wrapGApps;
 
   emacsPgtkGccPackages = final.lib.dontRecurseIntoAttrs (final.emacsPackagesFor final.emacsPgtkGcc);
 }
