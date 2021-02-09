d2086f9350b76e15a22e31f26557dc44a8af2cbd
Tony Olagbaiye <me@fron.io>
2021-02-09 21:32:32 +0000

    emacs(webkit): use init to sort out env

--- emacs/webkit.nix
+++ emacs/webkit.nix
@@ -1,12 +1,34 @@
 { config, lib, usr, pkgs, domains, ... }:
 
 {
-  emacs-loader.webkit = {
+  emacs-loader.webkit = let
+    env = {
+      GIO_EXTRA_MODULES = with pkgs; [
+        "${glib-networking}/lib/gio/modules"
+      ];
+      GST_PLUGIN_SYSTEM_PATH_1_0 = with pkgs.gst_all_1; [
+        "${gstreamer}/lib/gstreamer-1.0"
+        "${gst-libav}/lib/gstreamer-1.0"
+        "${gst-plugins-base}/lib/gstreamer-1.0"
+        "${gst-plugins-good}/lib/gstreamer-1.0"
+        "${gst-plugins-bad}/lib/gstreamer-1.0"
+        "${gst-plugins-ugly}/lib/gstreamer-1.0"
+      ];
+    };
+  in {
     demand = true;
     package = epkgs: epkgs.emacs-webkit;
     bind = {
       "M-*" = "webkit";
     };
+    init = ''
+      ${lib.concatMapStringsSep "" ({ name, value }: ''
+        (setenv "${name}"
+                (let ((cur (getenv "${name}"))
+                      (new "${lib.concatStringsSep ":" value}"))
+                  (if cur (concat new ":" cur) new)))
+      '') (builtins.mapAttrs lib.nameValuePair env)}
+    '';
     config = ''
       (with-eval-after-load 'evil
         (require 'evil-collection-webkit)
