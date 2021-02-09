24a8da7557750526dde8b94a833f6de32d9bce2c
Tony Olagbaiye <me@fron.io>
2021-02-09 21:25:58 +0000

    autosave emacs/webkit.nix after save

--- emacs/webkit.nix
+++ emacs/webkit.nix
@@ -3,16 +3,16 @@
 {
   emacs-loader.webkit = let
     env = {
-      GIO_EXTRA_MODULES = [
-        "${pkgs.glib-networking}/lib/gio/modules"
+      GIO_EXTRA_MODULES = with pkgs; [
+        "${glib-networking}/lib/gio/modules"
       ];
-      GST_PLUGIN_SYSTEM_PATH_1_0 = [
-        "${pkgs.gst_all_1.gstreamer}/lib/gstreamer-1.0"
-        "${pkgs.gst_all_1.gst-libav}/lib/gstreamer-1.0"
-        "${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0"
-        "${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0"
-        "${pkgs.gst_all_1.gst-plugins-bad}/lib/gstreamer-1.0"
-        "${pkgs.gst_all_1.gst-plugins-ugly}/lib/gstreamer-1.0"
+      GST_PLUGIN_SYSTEM_PATH_1_0 = pkgs.gst_all_1; [
+        "${gstreamer}/lib/gstreamer-1.0"
+        "${gst-libav}/lib/gstreamer-1.0"
+        "${gst-plugins-base}/lib/gstreamer-1.0"
+        "${gst-plugins-good}/lib/gstreamer-1.0"
+        "${gst-plugins-bad}/lib/gstreamer-1.0"
+        "${gst-plugins-ugly}/lib/gstreamer-1.0"
       ];
     };
   in {
