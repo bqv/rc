3051753bfcb6fdb3f0211d745e8472b2d0236059
Tony Olagbaiye <me@fron.io>
2021-02-13 16:10:28 +0000

    autosave emacs/evil.nix after save

--- emacs/evil.nix
+++ emacs/evil.nix
@@ -6,9 +6,9 @@
     diminish = [ "undo-tree-mode" ];
     config = ''
       (setq evil-default-cursor (quote (t "#750000"))
-          evil-visual-state-cursor '("#880000" box)
-          evil-insert-state-cursor '("#00CC00" box)
-          evil-normal-state-cursor '("#0088CC" box))
+            evil-visual-state-cursor '("#880000" box)
+            evil-insert-state-cursor '("#00CC00" box)
+            evil-normal-state-cursor '("#0088CC" box))
       ;(advice-add 'global-set-key :after
       ;            (lambda (old-fn &rest args)
       ;                     (apply 'evil-global-set-key args)))
@@ -32,6 +32,8 @@
       (define-key evil-insert-state-map (kbd "C-y") 'yank)
       (define-key evil-normal-state-map (kbd "C-w") 'kill-region)
       (define-key evil-visual-state-map (kbd "C-w") 'kill-region)
+
+      (setq evil-want-keybinding nil)
 
       ;; Override :q to kill buffer only
       (evil-define-command evil-quit (&optional force)
