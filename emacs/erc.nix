b18b6a7d52dce65bf143aee78c32191c345feef5
Tony Olagbaiye <me@fron.io>
2021-02-16 03:41:39 +0000

    autosave emacs/erc.nix after save

--- emacs/erc.nix
+++ emacs/erc.nix
@@ -90,8 +90,19 @@ Will not connect if we already have a connection to NETWORK.")
       (weechat-relay-send-command
        "infolist buffer"
        (lambda (infolist)
-         (mapcar (lambda (name) (erc-weechat-make-connect name (intern name))) (seq-uniq (mapcar #'cadr (seq-filter (lambda (segment) (equal "irc" (car segment))) (mapcar (lambda (buffer) (split-string (assoc-default "full_name" buffer) "\\.")) (car infolist))))))
-         ))
+         (mapcar
+          (lambda (name)
+            (print `(erc-weechat-make-connect name (intern name)))
+            (erc-weechat-make-connect name (intern name)))
+          (seq-uniq
+           (mapcar #'cadr
+                   (seq-filter
+                    (lambda (segment)
+                      (equal "irc" (car segment)))
+                    (mapcar
+                     (lambda (buffer)
+                       (split-string (assoc-default "full_name" buffer) "\\."))
+                     (car infolist))))))))
     '';
   };
   emacs.loader.erc-image = {
