cbea66ad7ce9f00f8824854220b09fdbd3c06537
Tony Olagbaiye <me@fron.io>
2021-02-16 04:19:50 +0000

    autosave emacs/erc.nix after save

--- emacs/erc.nix
+++ emacs/erc.nix
@@ -102,8 +102,7 @@ Will not connect if we already have a connection to NETWORK.")
                                          (assoc-default "full_name" buffer)
                                          "\\."))
                                       (car infolist))))))
-               (eval `(erc-weechat-make-connect ,name ,(intern name)))
-               (erc-weechat-connect name (intern name))))))
+               (eval `(erc-weechat-make-connect ,name ,(intern name)))))))
         (add-hook 'weechat-connect-hook #'erc-weechat-fetch-networks))
     '';
   };
