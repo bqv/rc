73861abcd625c0ca2c84348c3a2aa0da90d962ae
Tony Olagbaiye <me@fron.io>
2021-02-16 04:27:42 +0000

    autosave emacs/erc.nix after save

--- emacs/erc.nix
+++ emacs/erc.nix
@@ -14,6 +14,7 @@
             '("JOIN" "KICK" "NICK" "PART" "QUIT" "MODE" "333" "353"
               "324" "329" "332" "447"))
       (setq erc-pcomplete-nick-postfix ": ")
+      (setq erc-track-enable-keybindings t)
 
       (add-hook 'erc-mode-hook #'outline-minor-mode)
       (add-hook 'erc-mode-hook
