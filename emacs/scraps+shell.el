3cfc6b1c28cbf24ab0c33e4cbafcf209b92ed697
Tony Olagbaiye <me@fron.io>
2021-02-20 15:26:23 +0000

    autosave emacs/scraps+shell.el after save

--- emacs/scraps+shell.el
+++ emacs/scraps+shell.el
@@ -13,51 +13,33 @@
   (async-shell-command (buffer-substring-no-properties start end)))
 (global-set-key (kbd "C-x &") #'async-shell-region)
 
-(setq ivy-shell-keymap
-      (let ((keymap (make-keymap)))
-        (define-key keymap (kbd "<RET>") `(lambda ()
-                                            (interactive)
-                                            (if (string-equal (ivy--input) "")
-                                                (ivy-done)
-                                              (ivy-immediate-done))))
-        (define-key keymap (kbd "<tab>") #'completion-at-point)
-        (define-key keymap (kbd "C-i") `(lambda ()
+(defvar ivy-shell-keymap
+  (let ((keymap (make-keymap)))
+    (define-key keymap (kbd "<RET>") `(lambda ()
+                                        (interactive)
+                                        (if (string-equal (ivy--input) "")
+                                            (ivy-done)
+                                          (ivy-immediate-done))))
+    (define-key keymap (kbd "<tab>") #'completion-at-point)
+    (define-key keymap (kbd "C-i") `(lambda ()
+                                      (interactive)
+                                      (add-to-list 'completion-at-point-functions
+                                                   'comint-completion-at-point)
+                                      (completion-at-point)))
+    (define-key keymap (kbd "<backtab>") #'ivy-insert-current)
+    (define-key keymap (kbd "<C-tab>") `(lambda ()
                                           (interactive)
-                                          (add-to-list 'completion-at-point-functions
-                                                       'comint-completion-at-point)
-                                          (completion-at-point)))
-        (define-key keymap (kbd "<backtab>") #'ivy-insert-current)
-        (define-key keymap (kbd "<C-tab>") `(lambda ()
-                                              (interactive)
-                                              (ivy-quit-and-run
-                                                (ivy-shell ,(not async)
-                                                           :initial-input ,initial-input))))
-        keymap))
+                                          (ivy-quit-and-run
+                                            (ivy-shell ,(not async)
+                                                       :initial-input ,initial-input))))
+    keymap))
 
 (setq enable-recursive-minibuffers t)
 (defun ivy-shell (&optional async &key initial-input)
   (ivy-read (format "[%s] shell: " (if async "async" "synch"))
             shell-command-history
             :caller 'ivy-shell
-            :keymap (let ((keymap (make-keymap)))
-                      (define-key keymap (kbd "<RET>") `(lambda ()
-                                                          (interactive)
-                                                          (if (string-equal (ivy--input) "")
-                                                              (ivy-done)
-                                                            (ivy-immediate-done))))
-                      (define-key keymap (kbd "<tab>") #'completion-at-point)
-                      (define-key keymap (kbd "C-i") `(lambda ()
-                                                        (interactive)
-                                                        (add-to-list 'completion-at-point-functions
-                                                                     'comint-completion-at-point)
-                                                        (completion-at-point)))
-                      (define-key keymap (kbd "<backtab>") #'ivy-insert-current)
-                      (define-key keymap (kbd "<C-tab>") `(lambda ()
-                                                            (interactive)
-                                                            (ivy-quit-and-run
-                                                              (ivy-shell ,(not async)
-                                                                         :initial-input ,initial-input))))
-                      keymap)
+            :keymap ivy-shell-keymap
             :action (if async #'async-shell-command #'shell-command)
             :initial-input (or initial-input "")
             :history 'shell-command-history))
@@ -88,25 +70,7 @@
   (ivy-read (format "[%s] term: " "vterm")
             shell-command-history
             :caller 'ivy-term
-            :keymap (let ((keymap (make-keymap)))
-                      (define-key keymap (kbd "<RET>") `(lambda ()
-                                                          (interactive)
-                                                          (if (string-equal (ivy--input) "")
-                                                              (ivy-done)
-                                                            (ivy-immediate-done))))
-                      (define-key keymap (kbd "<tab>") #'completion-at-point)
-                      (define-key keymap (kbd "C-i") `(lambda ()
-                                                        (interactive)
-                                                        (add-to-list 'completion-at-point-functions
-                                                                     'comint-completion-at-point)
-                                                        (completion-at-point)))
-                      (define-key keymap (kbd "<backtab>") #'ivy-insert-current)
-                      (define-key keymap (kbd "<C-tab>") `(lambda ()
-                                                            (interactive)
-                                                            (ivy-quit-and-run
-                                                              (ivy-shell ,(not async)
-                                                                         :initial-input ,initial-input))))
-                      keymap)
+            :keymap ivy-shell-keymap
             :action #'vterm-shell-command
             :initial-input (or initial-input "")
             :history 'shell-command-history))
