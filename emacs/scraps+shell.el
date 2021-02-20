22dc2888c789a052d4419c967d4e5629c44583a9
Tony Olagbaiye <me@fron.io>
2021-02-20 15:37:45 +0000

    emacs(shell): ivy-term!

--- emacs/scraps+shell.el
+++ emacs/scraps+shell.el
@@ -13,30 +13,33 @@
   (async-shell-command (buffer-substring-no-properties start end)))
 (global-set-key (kbd "C-x &") #'async-shell-region)
 
+(defun ivy-shell-keymap (&optional async &key initial-input)
+  (let ((keymap (make-keymap)))
+    (define-key keymap (kbd "<RET>") `(lambda ()
+                                        (interactive)
+                                        (if (string-equal (ivy--input) "")
+                                            (ivy-done)
+                                          (ivy-immediate-done))))
+    (define-key keymap (kbd "<tab>") #'completion-at-point)
+    (define-key keymap (kbd "C-i") `(lambda ()
+                                      (interactive)
+                                      (add-to-list 'completion-at-point-functions
+                                                   'comint-completion-at-point)
+                                      (completion-at-point)))
+    (define-key keymap (kbd "<backtab>") #'ivy-insert-current)
+    (define-key keymap (kbd "<C-tab>") `(lambda ()
+                                          (interactive)
+                                          (ivy-quit-and-run
+                                            (ivy-shell ,(not async)
+                                                       :initial-input ,initial-input))))
+    keymap))
+
 (setq enable-recursive-minibuffers t)
 (defun ivy-shell (&optional async &key initial-input)
   (ivy-read (format "[%s] shell: " (if async "async" "synch"))
             shell-command-history
             :caller 'ivy-shell
-            :keymap (let ((keymap (make-keymap)))
-                      (define-key keymap (kbd "<RET>") `(lambda ()
-                                                          (interactive)
-                                                          (if (string-equal (ivy--input) "")
-                                                              (ivy-done)
-                                                            (ivy-immediate-done))))
-                      (define-key keymap (kbd "<tab>") #'completion-at-point)
-                      (define-key keymap (kbd "C-i") `(lambda ()
-                                                        (interactive)
-                                                        (add-to-list 'completion-at-point-functions
-                                                                     'comint-completion-at-point)
-                                                        (completion-at-point)))
-                      (define-key keymap (kbd "<backtab>") #'ivy-insert-current)
-                      (define-key keymap (kbd "<C-tab>") `(lambda ()
-                                                            (interactive)
-                                                            (ivy-quit-and-run
-                                                              (ivy-shell ,(not async)
-                                                                         :initial-input ,initial-input))))
-                      keymap)
+            :keymap (ivy-shell-keymap async :initial-input initial-input)
             :action (if async #'async-shell-command #'shell-command)
             :initial-input (or initial-input "")
             :history 'shell-command-history))
@@ -62,6 +65,19 @@
 (global-set-key (kbd "M-&") #'ivy-shell-async)
 (global-set-key (kbd "`") #'ivy-shell-async)
 (evil-define-key 'normal 'global (kbd "`") #'ivy-shell-async)
+
+(defun ivy-term (&optional &key initial-input)
+  (interactive)
+  (ivy-read (format "[%s] term: " "vterm")
+            shell-command-history
+            :caller 'ivy-term
+            :keymap (ivy-shell-keymap :initial-input initial-input)
+            :action #'vterm-shell-command
+            :initial-input (or initial-input "")
+            :history 'shell-command-history))
+(add-hook 'after-init-hook
+          (lambda (&rest _)
+            (global-set-key (kbd "C-x M-&") #'ivy-term)))
 
 (provide 'scraps+shell)
 ;;; scraps+shell.el ends here
