c7268486ee633490c6ed99929192e3c13b940eed
Tony Olagbaiye <me@fron.io>
2021-02-20 15:22:46 +0000

    autosave emacs/scraps+shell.el after save

--- emacs/scraps+shell.el
+++ emacs/scraps+shell.el
@@ -63,5 +63,32 @@
 (global-set-key (kbd "`") #'ivy-shell-async)
 (evil-define-key 'normal 'global (kbd "`") #'ivy-shell-async)
 
+(defun ivy-term (&key initial-input)
+  (ivy-read (format "[%s] term: " "vterm")
+            shell-command-history
+            :caller 'ivy-term
+            :keymap (let ((keymap (make-keymap)))
+                      (define-key keymap (kbd "<RET>") `(lambda ()
+                                                          (interactive)
+                                                          (if (string-equal (ivy--input) "")
+                                                              (ivy-done)
+                                                            (ivy-immediate-done))))
+                      (define-key keymap (kbd "<tab>") #'completion-at-point)
+                      (define-key keymap (kbd "C-i") `(lambda ()
+                                                        (interactive)
+                                                        (add-to-list 'completion-at-point-functions
+                                                                     'comint-completion-at-point)
+                                                        (completion-at-point)))
+                      (define-key keymap (kbd "<backtab>") #'ivy-insert-current)
+                      (define-key keymap (kbd "<C-tab>") `(lambda ()
+                                                            (interactive)
+                                                            (ivy-quit-and-run
+                                                              (ivy-shell ,(not async)
+                                                                         :initial-input ,initial-input))))
+                      keymap)
+            :action #'vterm-shell-command
+            :initial-input (or initial-input "")
+            :history 'shell-command-history))
+
 (provide 'scraps+shell)
 ;;; scraps+shell.el ends here
