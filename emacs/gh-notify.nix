6f85d0442644d7c47ba2779dee28fb443060c6b2
Tony Olagbaiye <me@fron.io>
2021-02-22 15:27:26 +0000

    emacs(gh-notify): evil emacs mode

--- emacs/gh-notify.nix
+++ emacs/gh-notify.nix
@@ -4,5 +4,9 @@
   emacs.loader.gh-notify = {
     demand = true;
     after = [ "magit" "forge" ];
+    config = ''
+      (with-eval-after-load 'evil
+        (add-to-list 'evil-emacs-state-modes 'gh-notify-mode))
+    '';
   };
 }
