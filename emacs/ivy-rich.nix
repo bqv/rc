af320680d42f6105398ddd6a5896bcf615506a7d
Tony Olagbaiye <me@fron.io>
2021-02-23 14:21:14 +0000

    autosave emacs/ivy-rich.nix after save

--- emacs/ivy-rich.nix
+++ emacs/ivy-rich.nix
@@ -5,10 +5,11 @@
     demand = true;
     after = [ "ivy" "counsel" ];
     config = ''
-      (setq ivy-rich-project-root-cache-mode t)
       (add-hook 'ivy-mode-hook 'ivy-rich-mode)
+      (ivy-rich-mode ivy-mode)
+
+      (setq ivy-rich-project-root-cache-mode t)
       (setq ivy-rich-path-style 'abbrev)
-      (ivy-rich-mode 1)
     '';
   };
 }
