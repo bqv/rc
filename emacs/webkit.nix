d907cf2f14df380ea917c08fa500584ed1648b0d
Tony Olagbaiye <me@fron.io>
2021-02-23 20:37:33 +0000

    autosave emacs/webkit.nix after save

--- emacs/webkit.nix
+++ emacs/webkit.nix
@@ -174,12 +174,9 @@
                      (ivy-rich-webkit-history-last-time (:face font-lock-comment-face))
                      (ivy-rich-webkit-history-visit-count (:align right)))
                    t #'ignore)
-      (ivy-rich-modify-columns ; ivy-rich-display-transformers-list
+      (memq
        'webkit-history-completing-read
-       '((ivy-rich-webkit-history-title (:width 0.4))
-         (ivy-rich-webkit-history-uri (:width 0.4))
-         (ivy-rich-webkit-history-last-time (:face font-lock-comment-face))
-         (ivy-rich-webkit-history-visit-count (:align right))))
+       ivy-rich-display-transformers-list)
     '';
   };
 }
