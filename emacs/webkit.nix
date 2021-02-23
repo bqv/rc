4784381704242e34bad5e211277aef2861763081
Tony Olagbaiye <me@fron.io>
2021-02-23 17:58:02 +0000

    autosave emacs/webkit.nix after save

--- emacs/webkit.nix
+++ emacs/webkit.nix
@@ -136,7 +136,8 @@
         (add-to-list 'ivy-rich-display-transformers-list
                      '(:columns
                        ((ivy-rich-candidate (:width 0.8))
-                        (ivy-rich-webkit-last-visited-time (:face font-lock-comment-face))))))
+                        (ivy-rich-webkit-last-visited-time (:face font-lock-comment-face))))
+                     t #'ignore))
     '';
   };
 }
