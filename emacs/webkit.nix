af7ed72d33aa149b04206e925b6f00af1940e624
Tony Olagbaiye <me@fron.io>
2021-02-23 18:28:11 +0000

    autosave emacs/webkit.nix after save

--- emacs/webkit.nix
+++ emacs/webkit.nix
@@ -130,7 +130,7 @@
               (progn (message "WebkitCandidate: %S" candidate) "?")
             (format-time-string "%Y-%m-%d %H:%M:%S" (nth 5 (file-attributes candidate))))))
 
-      (ivy-rich-modify-columns ; ivy-rich-display-transformers-alist
+      (ivy-rich-modify-columns ; ivy-rich-display-transformers-list
        'webkit-history-completing-read
        '((ivy-rich-candidate (:width 0.8))
          (ivy-rich-webkit-last-visited-time (:face font-lock-comment-face))))
