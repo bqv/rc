db76f18fe30902751babfd5876c0ca482bbc58fa
Tony Olagbaiye <me@fron.io>
2021-02-23 22:46:55 +0000

    autosave emacs/webkit.nix after save

--- emacs/webkit.nix
+++ emacs/webkit.nix
@@ -157,14 +157,6 @@
                (value (plist-get data 'webkit-last-time)))
           (if value (format-time-string "%Y-%m-%d %H:%M:%S" value) "?")))
 
-       (setcar (cdr (plist-get ivy-rich-display-transformers-list
-                        'webkit-history-completing-read))
-               '(
-                 (ivy-rich-webkit-history-title (:width 0.4))
-                 (ivy-rich-webkit-history-uri (:width 0.4 :face font-lock-string-face))
-                 (ivy-rich-webkit-history-last-time (:width 0.1 :face font-lock-comment-face))
-                 (ivy-rich-webkit-history-visit-count (:width 0.1 :align right :face font-lock-constant-face))
-                ))
       (add-to-list ivy-rich-display-transformers-list
                    'webkit-history-completing-read
                    t #'ignore)
@@ -172,7 +164,7 @@
                    '((ivy-rich-webkit-history-title (:width 0.4))
                      (ivy-rich-webkit-history-uri (:width 0.4 :face font-lock-string-face))
                      (ivy-rich-webkit-history-last-time (:width 0.1 :face font-lock-comment-face))
-                     (ivy-rich-webkit-history-visit-count (:width 0.1 :align right :face font-lock-constant-face)))
+                     (ivy-rich-webkit-history-visit-count (:align right :face font-lock-constant-face)))
                    t #'ignore)
       (memq
        'webkit-history-completing-read
