74ce78cd5207ace705eecb42e8d9a2483bd8c561
Tony Olagbaiye <me@fron.io>
2021-02-23 18:41:23 +0000

    autosave emacs/webkit.nix after save

--- emacs/webkit.nix
+++ emacs/webkit.nix
@@ -134,7 +134,7 @@
       (defun ivy-rich-webkit-last-visited-time (candidate)
         (let ((candidate (expand-file-name candidate ivy--directory)))
           (if (or (file-remote-p candidate) (not (file-exists-p candidate)))
-              (progn (message "WebkitCandidate: %S" candidate) "?")
+              (progn (message "WebkitCandidate: %s" (prin1 candidate)) "?")
             (format-time-string "%Y-%m-%d %H:%M:%S" (nth 5 (file-attributes candidate))))))
 
       (ivy-rich-modify-columns ; ivy-rich-display-transformers-list
