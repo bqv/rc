ef92a0581ace9891c76073590ac3e2f9a395fdd4
Tony Olagbaiye <me@fron.io>
2021-02-23 14:37:32 +0000

    autosave emacs/ivy-rich.nix after save

--- emacs/ivy-rich.nix
+++ emacs/ivy-rich.nix
@@ -9,12 +9,14 @@
       (defvar counsel-mode nil)
       (defvar counsel-projectile-mode nil)
 
-      (add-hook 'ivy-mode-hook 'ivy-rich-mode)
-      (ivy-rich-mode ivy-mode)
+      (defun maybe-restart-counsel-projectile ()
+        (when counsel-projectile-mode
+          (counsel-projectile-mode nil))
+        (counsel-projectile-mode t))
 
-      (when counsel-projectile-mode
-        (counsel-projectile-mode nil))
-      (counsel-projectile-mode t)
+      (add-hook 'ivy-mode-hook 'ivy-rich-mode)
+      (add-hook 'ivy-rich-mode-hook 'maybe-restart-counsel-projectile)
+      (ivy-rich-mode ivy-mode)
 
       (setq ivy-rich-project-root-cache-mode t)
       (setq ivy-rich-path-style 'abbrev)
@@ -29,6 +31,7 @@
     after = [ "ivy" "counsel" "all-the-icons" ];
     config = ''
       (add-hook 'ivy-mode-hook 'all-the-icons-ivy-rich-mode)
+      (add-hook 'all-the-icons-ivy-rich-mode-hook 'maybe-restart-counsel-projectile)
       (all-the-icons-ivy-rich-mode ivy-mode)
     '';
   };
