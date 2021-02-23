6f9f3895afd942b45bb309a367680e46d8c05e68
Tony Olagbaiye <me@fron.io>
2021-02-23 17:31:17 +0000

    autosave emacs/webkit.nix after save

--- emacs/webkit.nix
+++ emacs/webkit.nix
@@ -129,11 +129,30 @@
               (progn (print candidate) "?")
             (format-time-string "%Y-%m-%d %H:%M:%S" (nth 5 (file-attributes candidate))))))
 
+      ((webkit-history-completing-read
+        (:columns ((ivy-rich-candidate (:width 0.8)) (ivy-rich-webkit-last-visited-time (:face font-lock-comment-face)))))
+       ivy-switch-buffer
+       (:columns ((ivy-switch-buffer-transformer (:width 0.35)) (ivy-rich-switch-buffer-size (:width 7)) (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right)) (ivy-rich-switch-buffer-major-mode (:width 12 :face warning)) (ivy-rich-switch-buffer-project (:width 0.18 :face success)) (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3)))))) :predicate (lambda (cand) (get-buffer cand)))
+       counsel-find-file
+       (:columns ((ivy-read-file-transformer) (ivy-rich-counsel-find-file-truename (:face font-lock-doc-face))))
+       counsel-M-x
+       (:columns ((counsel-M-x-transformer (:width 0.4)) (ivy-rich-counsel-function-docstring (:face font-lock-doc-face))))
+       counsel-describe-function
+       (:columns ((counsel-describe-function-transformer (:width 0.4)) (ivy-rich-counsel-function-docstring (:face font-lock-doc-face))))
+       counsel-describe-variable
+       (:columns ((counsel-describe-variable-transformer (:width 0.4)) (ivy-rich-counsel-variable-docstring (:face font-lock-doc-face))))
+       counsel-recentf
+       (:columns ((ivy-rich-candidate (:width 0.8)) (ivy-rich-file-last-modified-time (:face font-lock-comment-face))))
+       counsel-bookmark
+       (:columns ((ivy-rich-candidate (:width 0.3)) (ivy-rich-bookmark-type) (ivy-rich-bookmark-info)))
+       package-install
+       (:columns ((ivy-rich-candidate (:width 30)) (ivy-rich-package-version (:width 16 :face font-lock-comment-face)) (ivy-rich-package-archive-summary (:width 7 :face font-lock-builtin-face)) (ivy-rich-package-install-summary (:face font-lock-doc-face)))))
       (add-to-list 'ivy-rich-display-transformers-list
-        '(webkit-history-completing-read
-          (:columns
-           ((ivy-rich-candidate (:width 0.8))
-            (ivy-rich-webkit-last-visited-time (:face font-lock-comment-face))))))
+                   'webkit-history-completing-read)
+      (add-to-list 'ivy-rich-display-transformers-list
+        '(:columns
+          ((ivy-rich-candidate (:width 0.8))
+           (ivy-rich-webkit-last-visited-time (:face font-lock-comment-face)))))
     '';
   };
 }
