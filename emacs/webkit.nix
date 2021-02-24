06c4e631018ed84863df0980045a7c7fab015d24
Tony Olagbaiye <me@fron.io>
2021-02-24 01:11:11 +0000

    autosave emacs/webkit.nix after save

--- emacs/webkit.nix
+++ emacs/webkit.nix
@@ -163,7 +163,7 @@
           (add-to-list ivy-rich-display-transformers-list
                        'webkit-history-completing-read
                        t #'ignore)
-          (add-to-list ivy-rich-display-transformers-list nil))
+          (add-to-list ivy-rich-display-transformers-list nil t #'ignore))
         (setcar (cdr (plist-get ivy-rich-display-transformers-list
                                 'webkit-history-completing-read))
                 '((ivy-rich-webkit-history-title (:width 0.4))
