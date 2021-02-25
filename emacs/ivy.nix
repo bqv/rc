475024b55718b999651bcd3f9bd78caf969383cc
Tony Olagbaiye <me@fron.io>
2021-02-25 02:15:37 +0000

    autosave emacs/ivy.nix after save

--- emacs/ivy.nix
+++ emacs/ivy.nix
@@ -9,7 +9,7 @@
         ; Compatibility hack
         arg)
       (setq-default ivy-initial-inputs-alist nil)
-     '
+
 (setq ivy-re-builders-alist '((t . orderless-ivy-re-builder)))
       (ivy-mode t)
       (add-hook 'after-init-hook
