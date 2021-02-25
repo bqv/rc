688c20018844984774a522dee48b0c3a03500625
Tony Olagbaiye <me@fron.io>
2021-02-25 02:13:49 +0000

    autosave emacs/orderless.nix after save

--- emacs/orderless.nix
+++ emacs/orderless.nix
@@ -4,9 +4,5 @@
   emacs.loader.orderless = {
     demand = true;
     after = [ "ivy" ];
-    config = ''
-      (setq ivy-re-builders-alist '((t . orderless-ivy-re-builder)))
-      ;(add-to-list 'completion-styles 'orderless-ivy-re-builder)
-    '';
   };
 }
