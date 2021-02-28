0c7f3b3a452c985b9d409343f34e8fce0977828d
Tony Olagbaiye <me@fron.io>
2021-02-28 20:54:22 +0000

    autosave emacs/scraps+misc.el after save

--- emacs/scraps+misc.el
+++ emacs/scraps+misc.el
@@ -1,15 +1,3 @@
-(defun bqv/pmbootstrap-log ()
-  "Open pmbootstrap log in a buffer."
-  (interactive)
-  (switch-to-buffer
-   (let* ((title "*pmbootstrap-log*")
-          (buffer (get-buffer title)))
-     (or buffer
-         (save-excursion
-           (with-current-buffer (window-buffer
-                                 (async-shell-command "pmbootstrap log"))
-             (rename-buffer title t)))))))
-
 (defun comint-sudo-run (program)
   "Run sudo PROGRAM in a Comint buffer and switch to it.
 See `comint-run'."
