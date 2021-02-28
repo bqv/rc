e72d859ac6987786f0c61d839113ad4016854f9c
Tony Olagbaiye <me@fron.io>
2021-02-28 20:58:34 +0000

    autosave tracked files before stage

--- emacs/scraps+web.el
+++
@@ -1,4 +0,0 @@
-(when (executable-find "xdg-open")
-  (with-eval-after-load 'browse-url
-    (setq browse-url-generic-program "xdg-open"
-          browse-url-browser-function 'browse-url-generic)))
