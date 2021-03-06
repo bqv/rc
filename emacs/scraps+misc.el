8b80f2363d60cd626d6aed52d93cfe42e35ab8cc
Tony Olagbaiye <me@fron.io>
2021-03-06 17:32:22 +0000

    autosave emacs/scraps+misc.el after stage

--- emacs/scraps+misc.el
+++ emacs/scraps+misc.el
@@ -1,12 +1,3 @@
-(defun comint-sudo-run (program)
-  "Run sudo PROGRAM in a Comint buffer and switch to it.
-See `comint-run'."
-  (declare (interactive-only make-comint))
-  (interactive "sRun program: ")
-  (let ((name (file-name-nondirectory program)))
-    (switch-to-buffer (make-comint name "sudo" nil program))
-    (run-hooks (intern-soft (concat "comint-" name "-hook")))))
-
 ;; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
 (defun rename-file-and-buffer (new-name)
   "Renames both current buffer and file it's visiting to NEW-NAME."
