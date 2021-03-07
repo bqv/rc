5ad05e68852f36586cec3a386a4c27b4f24f2dfb
Tony Olagbaiye <me@fron.io>
2021-03-07 02:17:56 +0000

    autosave emacs/default.nix after save

--- emacs/default.nix
+++ emacs/default.nix
@@ -5,4 +5,13 @@
     (lib.mapAttrsToList (file: _: ./. + "/${file}")  # strings to paths
       (lib.filterAttrs (f: _: f != ".dir-locals.el") # remove bad files
         (builtins.readDir ./.)));                    # enumerate folder
+
+  emacs.loader.scraps = {
+    package = lib.const null;
+    script = epkgs: ''
+      ${builtins.readFile ./scraps+bufmgmt.el}
+      ${builtins.readFile ./scraps+misc.el}
+      ${builtins.readFile ./scraps+shell.el}
+    '';
+  };
 }
