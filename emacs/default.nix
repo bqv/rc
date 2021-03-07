9bdb6fc89b86a138d64679d032a9d852956e3ce0
Tony Olagbaiye <me@fron.io>
2021-03-07 02:21:06 +0000

    autosave emacs/default.nix after save

--- emacs/default.nix
+++ emacs/default.nix
@@ -9,9 +9,12 @@
   emacs.loader.scraps = {
     package = lib.const null;
     script = epkgs: ''
-      ${builtins.readFile ./scraps+bufmgmt.el}
-      ${builtins.readFile ./scraps+misc.el}
-      ${builtins.readFile ./scraps+shell.el}
+      (defun bqv/load-scraps ()
+        ${builtins.readFile ./scraps+bufmgmt.el}
+        ${builtins.readFile ./scraps+misc.el}
+        ${builtins.readFile ./scraps+shell.el}
+        t)
+      (add-to-list 'emacs-startup-hook #'bqv/load-scraps)
     '';
   };
 }
