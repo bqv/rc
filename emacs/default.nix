66cb1b8fbb81515dbb1f7b8b1ba489d5312cc6fc
Tony Olagbaiye <me@fron.io>
2021-03-07 02:22:58 +0000

    emacs: rejig init for dumping

--- emacs/default.nix
+++ emacs/default.nix
@@ -5,4 +5,16 @@
     (lib.mapAttrsToList (file: _: ./. + "/${file}")  # strings to paths
       (lib.filterAttrs (f: _: f != ".dir-locals.el") # remove bad files
         (builtins.readDir ./.)));                    # enumerate folder
+
+  emacs.loader.scraps = {
+    package = lib.const null;
+    script = epkgs: ''
+      (defun bqv/load-scraps ()
+        ${builtins.readFile ./scraps+bufmgmt.el}
+        ${builtins.readFile ./scraps+misc.el}
+        ${builtins.readFile ./scraps+shell.el}
+        t)
+      (add-to-list 'emacs-startup-hook #'bqv/load-scraps)
+    '';
+  };
 }
