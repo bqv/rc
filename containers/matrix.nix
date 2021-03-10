5fdad87d43635a4fab8dc0e25320ea443819e57b
Tony Olagbaiye <me@fron.io>
2021-03-10 22:31:20 +0000

    autosave containers/matrix.nix after stage

--- containers/matrix.nix
+++ containers/matrix.nix
@@ -43,19 +43,17 @@ in {
           environment.systemPackages = with pkgs; [ screen ];
           services.matrix-dendrite = rec {
             enable = true;
-            #environmentFile = null;
             generatePrivateKey = true;
             generateTls = false;
             httpPort = 8008;
-            settings.global = let
+            settings = let
               mkDb = with {
                 authority = "dendrite";
                 hostname = hostAddress;
-              }; name: "postgresql://${authority}@${hostname}/dendrite-${name}";
+              }; name: "postgresql://${authority}@${hostname}/dendrite-${name}?sslmode=disable";
             in {
-              api_registration_disabled = false;
-              server_name = "${usr.secrets.domains.srvc}:${toString httpPort}";
-              kafka.naffka_database.connection_string = mkDb "naffka";
+              global.server_name = "${usr.secrets.domains.srvc}";
+              global.kafka.naffka_database.connection_string = mkDb "naffka";
               app_service_api.database.connection_string = mkDb "appservice";
               federation_sender.database.connection_string = mkDb "federationsender";
               key_server.database.connection_string = mkDb "keyserver";
@@ -67,20 +65,40 @@ in {
               user_api.account_database.connection_string = mkDb "userapi-accounts";
               user_api.device_database.connection_string = mkDb "userapi-devices";
               client_api = {
+                registration_disabled = false;
                 inherit (usr.secrets.matrix.synapse) registration_shared_secret;
               };
               mscs.mscs = [ "msc2946" ];
-             #public_baseurl = "https://matrix.${usr.secrets.domains.srvc}/";
             };
             tlsCert = "/var/lib/acme/${usr.secrets.domains.srvc}/fullchain.pem";
             tlsKey = "/var/lib/acme/${usr.secrets.domains.srvc}/key.pem";
           };
 
+          services.nginx.enable = true;
+          services.nginx.virtualHosts.wellknown-matrix = {
+            locations = {
+             #"/.well-known/matrix/server".extraConfig = ''
+             #  return 200 '{ "m.server": "${cfg.nginxVhost}:443" }';
+             #'';
+             #"/.well-known/matrix/client".extraConfig = ''
+             #  return 200 '{ "m.homeserver": { "base_url": "https://${cfg.nginxVhost}" } }';
+             #'';
+             #"/_matrix".proxyPass = "http://localhost:8008";
+              "/server".extraConfig = ''
+                return 200 '{ "m.server": "${usr.secrets.domains.srvc}:443" }';
+              '';
+              "/client".extraConfig = ''
+                return 200 '{ "m.homeserver": { "base_url": "https://m.${usr.secrets.domains.srvc}" } }';
+              '';
+            };
+          };
+
+          systemd.services.matrix-dendrite = {
+            serviceConfig.Group = "keys";
+          };
+
           networking.firewall.enable = false;
 
-         #users.users.matrix-synapse.extraGroups = [
-         #  "keys"
-         #];
          #users.users.construct.extraGroups = [
          #  "keys"
          #];
