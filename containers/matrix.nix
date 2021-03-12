39717b8ba896140c00d0e77f82a4f963d84ebfce
Tony Olagbaiye <me@fron.io>
2021-03-12 07:56:42 +0000

    autosave containers/matrix.nix after save

--- containers/matrix.nix
+++ containers/matrix.nix
@@ -54,24 +54,19 @@ in {
               room_server.database.connection_string = mkDb "roomserver";
               signing_key_server.database.connection_string = mkDb "signingkeyserver";
               signing_key_server.prefer_direct_fetch = false;
-              signing_key_server.key_perspectives = [
-                {
-                  server_name = "cs.xa0.uk";
-                  keys = [
-                    { key_id = "ed25519:1UniuHrK";
-                      public_key = "1UniuHrKHqhudGA5H61Btb11La5/2f6LhRzijQCLzjE";
-                    }
-                  ];
-                }
-                {
-                  server_name = "sn.xa0.uk";
-                  keys = [
-                    { key_id = "ed25519:a_zUxc";
-                      public_key = "f5ScwCaM6EjaAIYIH0+buuZKH4hCiIbXP9wsui6wQ20";
-                    }
-                  ];
-                }
-              ];
+              signing_key_server.key_perspectives = [{
+                server_name = "cs.xa0.uk";
+                keys = [{
+                  key_id = "ed25519:1UniuHrK";
+                  public_key = "1UniuHrKHqhudGA5H61Btb11La5/2f6LhRzijQCLzjE";
+                }];
+              } {
+                server_name = "sn.xa0.uk";
+                keys = [{
+                  key_id = "ed25519:a_zUxc";
+                  public_key = "f5ScwCaM6EjaAIYIH0+buuZKH4hCiIbXP9wsui6wQ20";
+                }];
+              }];
               sync_api.database.connection_string = mkDb "syncapi";
               user_api.account_database.connection_string = mkDb "userapi-accounts";
               user_api.device_database.connection_string = mkDb "userapi-devices";
