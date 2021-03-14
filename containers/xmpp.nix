3e9d6e2eb8270ed0c349989cfbe09dd178ba8026
Tony Olagbaiye <me@fron.io>
2021-03-14 10:53:17 +0000

    autosave containers/xmpp.nix after save

--- containers/xmpp.nix
+++ containers/xmpp.nix
@@ -60,9 +60,9 @@ in {
             uploadHttp = {
               domain = "xmpp.xa0.uk";
             };
-            disco_items = [{
-              url = "xmpp.xa0.uk";
-            }];
+           #disco_items = [{
+           #  url = "xmpp.xa0.uk";
+           #}];
           };
 
           networking.firewall.enable = false;
