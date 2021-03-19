363646ff0be65c2678066cbbb2a9ba99d1dfac69
Tony Olagbaiye <me@fron.io>
2021-03-19 02:52:11 +0000

    autosave emacs/jabber.nix after save

--- emacs/jabber.nix
+++ emacs/jabber.nix
@@ -4,10 +4,11 @@
   emacs.loader.jabber = {
     demand = true;
     package = epkgs: epkgs.jabber.overrideAttrs (drv: {
-      src = pkgs.fetchGit {
+      src = pkgs.fetchgit {
         url = "https://tildegit.org/wgreenhouse/emacs-jabber.git";
-        rev = "master";
-        sha256 = "null";
+        rev = "69d6122858f036958323c50f81423389b0f91d69";
+        sha256 = "0vg5zwcc7afwbjbravbbwjbfpd3jz2i4zckmnlpg5cabidlxwjzr";
+        # date = 2021-03-16T20:16:52-04:00;
       };
     });
     config = ''
