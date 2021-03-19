8337a7196fc5dc3ccadb452a2cddd184a8bf773d
Tony Olagbaiye <me@fron.io>
2021-03-19 02:45:34 +0000

    autosave emacs/jabber.nix after save

--- emacs/jabber.nix
+++ emacs/jabber.nix
@@ -3,6 +3,9 @@
 {
   emacs.loader.jabber = {
     demand = true;
+    package = epkgs: epkgs.jabber.overrideAttrs (drv: {
+      src = "https://tildegit.org/wgreenhouse/emacs-jabber.git";
+    });
     config = ''
       nil
     '';
