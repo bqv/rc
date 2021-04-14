54c1f16b13c625b0e0ff4c53f757c628a2d23099
Tony Olagbaiye <me@fron.io>
2021-04-14 22:09:02 +0100

    autosave flake.nix after save

--- flake.nix
+++ flake.nix
@@ -248,7 +248,6 @@
                   inherit (withFunkwhaleFlake) funkwhale;
                   inherit (withEmacsFlake.withSelfFlake.withEmacs) emacsPgtkGcc emacsPgtkGccClient emacsPgtkGccPackages;
                   giara = builtins.trace "pkgs.giara: broken, for now" prev.hello; #inherit (withGiara) giara;
-                  inherit (withLbry) lbry;
                   inherit (withCordless) cordless;
                   inherit (withLarge.withHnix) hnix;
                   inherit (withNix) nixFlakes nix-static nix-ipfs;
