cb91f8a74fd8675b0e3333754b50db4e1a00af92
Tony Olagbaiye <me@fron.io>
2021-06-05 17:11:53 +0100

    system(delta): add iwd

--- rc/system/delta.scm
+++ rc/system/delta.scm
@@ -1,53 +1,54 @@
 (define-module (rc system delta)
-	       #:use-module (gnu)
-	       #:use-module (gnu packages linux)
-	       #:use-module (gnu system nss)
-	       #:use-module (nongnu packages linux)
-	       #:use-module (nongnu packages mozilla)
-	       #:use-module (nongnu system linux-initrd)
-	       #:use-module (srfi srfi-1)
-	       #:use-module (gnu services desktop)
-	       #:use-module (gnu services xorg)
-	       #:use-module (gnu services sddm)
-	       #:use-module (gnu services shepherd)
-	       #:use-module (gnu services ssh)
-	       #:use-module (gnu services networking)
-	       #:use-module (gnu services nix)
-	       #:use-module (gnu services vpn)
-	       #:use-module (gnu packages certs)
-	       #:use-module (gnu packages gnome)
-	       #:use-module (gnu packages vim)
-	       #:use-module (gnu packages admin)
-	       #:use-module (gnu packages chromium)
-	       #:use-module (gnu packages file)
-	       #:use-module (gnu packages ssh)
-	       #:use-module (gnu packages rust-apps)
-	       #:use-module (gnu packages tmux)
-	       #:use-module (gnu packages dvtm)
-	       #:use-module (gnu packages version-control)
-	       #:use-module (gnu packages shells)
-	       #:use-module (gnu packages networking)
-	       #:use-module (gnu packages compression)
-	       #:use-module (gnu packages cpio)
-	       #:use-module (gnu packages vpn)
-	       #:use-module (gnu packages python)
-	       #:use-module (gnu packages web-browsers)
-	       #:use-module (gnu packages curl)
-	       #:use-module (gnu packages screen)
-	       #:use-module (gnu packages emacs)
-	       #:use-module (gnu packages emacs-xyz)
-	       #:use-module (gnu packages xorg)
-	       #:use-module (gnu packages wm)
-	       #:use-module (gnu packages ipfs)
-	       #:use-module (gnu packages messaging)
-	       #:use-module (gnu packages irc)
-	       #:use-module (gnu packages web)
-	       #:use-module (gnu packages rsync)
-	       #:use-module (gnu packages gnupg)
-	       #:use-module (gnu packages terminals)
-	       #:use-module (gnu packages xdisorg)
-	       #:use-module (flat packages emacs)
-	       #:export (os))
+               #:use-module (gnu)
+               #:use-module (gnu packages linux)
+               #:use-module (gnu system nss)
+               #:use-module (nongnu packages linux)
+               #:use-module (nongnu packages mozilla)
+               #:use-module (nongnu system linux-initrd)
+               #:use-module (srfi srfi-1)
+               #:use-module (gcrypt pk-crypto)
+               #:use-module (gnu services desktop)
+               #:use-module (gnu services xorg)
+               #:use-module (gnu services sddm)
+               #:use-module (gnu services shepherd)
+               #:use-module (gnu services ssh)
+               #:use-module (gnu services networking)
+               #:use-module (gnu services nix)
+               #:use-module (gnu services vpn)
+               #:use-module (gnu packages certs)
+               #:use-module (gnu packages gnome)
+               #:use-module (gnu packages vim)
+               #:use-module (gnu packages admin)
+               #:use-module (gnu packages chromium)
+               #:use-module (gnu packages file)
+               #:use-module (gnu packages ssh)
+               #:use-module (gnu packages rust-apps)
+               #:use-module (gnu packages tmux)
+               #:use-module (gnu packages dvtm)
+               #:use-module (gnu packages version-control)
+               #:use-module (gnu packages shells)
+               #:use-module (gnu packages networking)
+               #:use-module (gnu packages compression)
+               #:use-module (gnu packages cpio)
+               #:use-module (gnu packages vpn)
+               #:use-module (gnu packages python)
+               #:use-module (gnu packages web-browsers)
+               #:use-module (gnu packages curl)
+               #:use-module (gnu packages screen)
+               #:use-module (gnu packages emacs)
+               #:use-module (gnu packages emacs-xyz)
+               #:use-module (gnu packages xorg)
+               #:use-module (gnu packages wm)
+               #:use-module (gnu packages ipfs)
+               #:use-module (gnu packages messaging)
+               #:use-module (gnu packages irc)
+               #:use-module (gnu packages web)
+               #:use-module (gnu packages rsync)
+               #:use-module (gnu packages gnupg)
+               #:use-module (gnu packages terminals)
+               #:use-module (gnu packages xdisorg)
+               #:use-module (flat packages emacs)
+               #:export (os))
 
 (define (os)
   (operating-system
@@ -75,11 +76,10 @@
     (firmware (cons* amdgpu-firmware linux-firmware
                      %base-firmware))
   
-    ;; Specify a mapped device for the encrypted root partition.
-    ;; The UUID is that returned by 'cryptsetup luksUUID'.
    ;(mapped-devices
    ; (list (mapped-device
    ;        (source (uuid "12345678-1234-1234-1234-123456789abc"))
+   ;        ;; The UUID is that returned by 'cryptsetup luksUUID'.
    ;        (target "my-root")
    ;        (type luks-device-mapping))))
   
@@ -141,12 +141,12 @@
     (packages (cons*
                 nss-certs ;; for HTTPS access
                 gvfs ;; for user mounts
-                vim htop firefox mosh ripgrep tmux dvtm git go-ipfs file
+                vim htop firefox mosh ripgrep tmux dvtm git go-ipfs file iwd
                 emacs-pgtk-native-comp neovim nyxt xterm sshfs tree curl screen jq
                 stumpwm wireguard emacs-evil emacs-ivy emacs-vterm emacs-geiser
                 efibootmgr dino weechat xinit irssi profanity poezio
-		ungoogled-chromium fish fish-foreign-env netcat rofi
-		xinit setxkbmap rsync gnupg sway awesome termite alacritty
+                ungoogled-chromium fish fish-foreign-env netcat rofi
+                xinit setxkbmap rsync gnupg sway awesome termite alacritty
                 %base-packages))
   
     (setuid-programs (cons*
@@ -156,10 +156,8 @@
                               root ALL=(ALL) ALL
                               %wheel ALL=(ALL) NOPASSWD:ALL\n"))
   
-    ;; Add GNOME and Xfce---we can choose at the log-in screen
-    ;; by clicking the gear.  Use the "desktop" services, which
-    ;; include the X11 log-in service, networking with
-    ;; NetworkManager, and more.
+    (name-service-switch %mdns-host-lookup-nss)
+  
     (services (cons* (service gnome-desktop-service-type)
                      (service xfce-desktop-service-type)
                      (service openssh-service-type
@@ -176,30 +174,37 @@
                               (ipfs-configuration))
                      (service nftables-service-type
                               (nftables-configuration
-                                (ruleset (plain-file "ruleset" "\
-  table inet filter {
-          chain input {
-                  type filter hook input priority filter; policy drop;
-                  ct state invalid drop
-                  ct state { established, related } accept
-                  iifname \"lo\" accept
-                  ip protocol icmp accept
-                  ip6 nexthdr ipv6-icmp accept
-                  tcp dport 22 accept
-                  udp dport 60000-65535 accept
-  		accept
-                  reject
-          }
-  
-          chain forward {
-                  type filter hook forward priority filter; policy drop;
-          }
-  
-          chain output {
-                  type filter hook output priority filter; policy accept;
-          }
-  }
-                                                               "))))
+                                (ruleset
+                                  (plain-file "ruleset" 
+                                              (let ((unlines
+                                                      (lambda* (#:rest l)
+                                                               (string-join l "\n"))))
+                                              (unlines
+                                                "table inet filter {"
+                                                (unlines
+                                                  "chain input {"
+                                                  (unlines
+                                                    "type filter hook input priority filter; policy drop;"
+                                                    "ct state invalid drop"
+                                                    "ct state { established, related } accept"
+                                                    "iifname \"lo\" accept"
+                                                    "ip protocol icmp accept"
+                                                    "ip6 nexthdr ipv6-icmp accept"
+                                                    "tcp dport 22 accept"
+                                                    "udp dport 60000-65535 accept"
+                                                    "accept");"reject")
+                                                  "}")
+                                                (unlines
+                                                  "chain forward {"
+                                                  (unlines
+                                                    "type filter hook forward priority filter; policy drop;")
+                                                  "}")
+                                                (unlines
+                                                  "chain output {"
+                                                  (unlines
+                                                    "type filter hook output priority filter; policy accept;")
+                                                  "}")
+                                                "}"))))))
                      (simple-service 'weechat shepherd-root-service-type
                                      (list (shepherd-service
                                              (documentation "Run the weechat daemon.")
@@ -209,10 +214,10 @@
                                                         (list #$(file-append weechat "/bin/weechat-headless")
                                                               "-d" "/var/lib/weechat")))
                                              (stop #~(make-kill-destructor)))))
-                    ;(service wpa-supplicant-service-type
-                    ;         (wpa-supplicant-configuration
-                    ;           (interface "wlo1")
-                    ;           (config-file "/etc/wpa_supplicant/wpa_supplicant.conf")))
+                     ;(service wpa-supplicant-service-type
+                     ;         (wpa-supplicant-configuration
+                     ;           (interface "wlo1")
+                     ;           (config-file "/etc/wpa_supplicant/wpa_supplicant.conf")))
                      (service sddm-service-type
                               (sddm-configuration
                                 (auto-login-user "leaf")
@@ -220,32 +225,40 @@
                                 (xorg-configuration
                                   (xorg-configuration
                                     (keyboard-layout keyboard-layout)))))
+                    ;(static-networking-service "enp4s0u1" "192.168.178.252"
+                    ;                           #:gateway "192.168.178.1"
+                    ;                           #:name-servers '("9.9.9.9"))
+                     (service dhcpd-service-type
+                              (dhcpd-configuration
+                                (config-file (plain-file "dhcpcd.conf" ""))
+                                (version "4o6")
+                                (interfaces '("eno2" "wlo1"))))
                     ;(service elogind-service-type
                     ;         (elogind-configuration))
-  		   (service wireguard-service-type
-  			    (wireguard-configuration
-  			      (addresses '("10.0.0.3/24"))
-  			      (peers
-  				(list
-  				  (wireguard-peer
-  				    (name "zeta")
-  				    (endpoint "163.172.7.233:51820")
-  				    (public-key "WbZqPcgSxWf+mNsWVbS+0JylysN9FKrRG9783wn1JAg=")
-  				    (allowed-ips '("10.0.0.1/32"))
-  				    (keep-alive 10))
-  				  (wireguard-peer
-  				    (name "theta")
-  				    (endpoint "192.168.178.101:51820")
-  				    (public-key "Itld9S83/URY8CR1ZsIfYRGK74/T0O5YbsHWcNpn2gE=")
-  				    (allowed-ips '("10.0.0.2/32"))
-  				    (keep-alive 10))
-  				  (wireguard-peer
-  				    (name "phi")
-  				    (endpoint "192.168.178.135:51820")
-  				    (public-key "kccZA+GAc0VStb28A+Kr0z8iPCWsiuRMfwHW391Qrko=")
-  				    (allowed-ips '("10.0.0.4/32"))
-  				    (keep-alive 10))))))
-  		   (simple-service 'no-eth shepherd-root-service-type
+                     (service wireguard-service-type
+                              (wireguard-configuration
+                                (addresses '("10.0.0.3/24"))
+                                (peers
+                                  (list
+                                    (wireguard-peer
+                                      (name "zeta")
+                                      (endpoint "163.172.7.233:51820")
+                                      (public-key "WbZqPcgSxWf+mNsWVbS+0JylysN9FKrRG9783wn1JAg=")
+                                      (allowed-ips '("10.0.0.1/32"))
+                                      (keep-alive 10))
+                                    (wireguard-peer
+                                      (name "theta")
+                                      (endpoint "192.168.178.101:51820")
+                                      (public-key "Itld9S83/URY8CR1ZsIfYRGK74/T0O5YbsHWcNpn2gE=")
+                                      (allowed-ips '("10.0.0.2/32"))
+                                      (keep-alive 10))
+                                    (wireguard-peer
+                                      (name "phi")
+                                      (endpoint "192.168.178.135:51820")
+                                      (public-key "kccZA+GAc0VStb28A+Kr0z8iPCWsiuRMfwHW391Qrko=")
+                                      (allowed-ips '("10.0.0.4/32"))
+                                      (keep-alive 10))))))
+                     (simple-service 'no-eth shepherd-root-service-type
                                      (list (shepherd-service
                                              (documentation "Set enp4s0u1 link down.")
                                              (provision '(no-eth))
@@ -254,32 +267,36 @@
                                                         (let ((ip (string-append #$iproute "/sbin/ip")))
                                                           (invoke ip "link" "set" "enp4s0u1" "down"))))
                                              (one-shot? #t))))
-                     (extra-special-file "/etc/doas.conf"
-                                         (plain-file "doas.conf" "\
-  # 'root' is allowed to do anything.
-  permit nopass keepenv root
-  
-  permit nopass    setenv { SSH_AUTH_SOCK  } :wheel   
-                                                                 "))
+                     (extra-special-file
+                       "/etc/doas.conf"
+                       (plain-file "doas.conf"
+                                   (string-join (list
+                                                  "permit nopass keepenv root" ; allowed to do anything
+                                                  "permit nopass setenv { SSH_AUTH_SOCK  } :wheel")
+                                                "\n")))
                      (modify-services
                        %desktop-services
                        (delete gdm-service-type)
-  		     (guix-service-type config =>
-  					(guix-configuration
-  					  (inherit config)
-  					  (substitute-urls
-  					    (append (list "https://mirror.brielmaier.net")
-  						    %default-substitute-urls))
-  					  (authorized-keys
-  					    (append (list (plain-file "key.pub" "\
-  (public-key 
-   (ecc 
-    (curve Ed25519)
-    (q #7514F8D729DB1935470A581CE3851ED9FD6F1F9BAFE1D8BEC77A931ADB7A4337#)
-    )
-   )
-  								                "))
-  						    %default-authorized-guix-keys)))))))
-  
-    ;; Allow resolution of '.local' host names with mDNS.
-    (name-service-switch %mdns-host-lookup-nss)))
+                       (delete network-manager-service-type)
+                       (guix-service-type config =>
+                                          (guix-configuration
+                                            (inherit config)
+                                            (substitute-urls
+                                              (append
+                                                (list "https://mirror.brielmaier.net")
+                                                %default-substitute-urls))
+                                            (authorized-keys
+                                              (append
+                                                (list (plain-file
+                                                        "mirror.brielmair.net.pub"
+                                                        (canonical-sexp->string
+                                                          (sexp->canonical-sexp
+                                                            '(public-key
+                                                               (ecc
+                                                                 (curve Ed25519)
+                                                                 (q #vu8(117 20 248 215 41 219 25 53
+                                                                         71 10 88 28 227 133 30 217
+                                                                         253 111 31 155 175 225 216 190
+                                                                         199 122 147 26 219 122 67 55))))
+                                                            ))))
+                                                %default-authorized-guix-keys)))))))))
