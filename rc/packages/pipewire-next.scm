0e7581f5a2982e4ff6195c59f96edbd3ad6aa72b
Tony Olagbaiye <me@fron.io>
2021-06-09 05:29:37 +0100

    packages: actually bump pipewire-next

--- rc/packages/pipewire-next.scm
+++ rc/packages/pipewire-next.scm
@@ -1,4 +1,6 @@
 (define-module (rc packages pipewire-next)
+               #:use-module (guix git-download)
+               #:use-module (guix utils)
                #:use-module (guix packages)
                #:use-module (gnu packages linux)
                #:use-module (gnu packages pulseaudio)
@@ -8,7 +10,27 @@
   (package
     (inherit pipewire-0.3)
     (version "0.3.29")
-    (source (package-source pipewire-0.3))))
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                     (url (git-reference-url (origin-uri (package-source pipewire-0.3))))
+                     (commit version)))
+              (file-name (git-file-name (package-name pipewire-0.3) version))
+              (sha256
+                (base32
+                  "16jjxcnahxqfcawz77ywx837ybhwzcivn7hgqb9cmlp1y2syy8gk"))))
+    (arguments
+     (map (lambda (a)
+            (if (list? a)
+                (map (lambda (f)
+                       (if (string? f)
+                           (string-replace-substring f
+                                                     "systemd=false"
+                                                     "systemd=disabled")
+                           f))
+                     a)
+                a))
+          (package-arguments pipewire-0.3)))))
 
 (define-public pulseaudio->pipewire
   (package-input-rewriting
